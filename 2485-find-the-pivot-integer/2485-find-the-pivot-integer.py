18ec393e0bb9a04659ab93949d0d80fce3ba79b0
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2022-11-27 14:49:16 +0530

    Time: 47 ms (91.67%), Space: 13.9 MB (8.33%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,9 +1,17 @@
 class Solution:
-    def pivotInteger(self, n: int) -> int:
-        total_sum = (1 + n) * n // 2
-        for x in range(1, n+1):
-            a = x * (1 + x) - x 
-            if a == total_sum:
-                return x
-            if a > total_sum:
-                return -1
+    def pivotInteger(self, n):
+        x = sqrt(n * (n + 1) // 2)
+        return -1 if x % 1 else int(x)
+
+
+# class Solution:
+#     def pivotInteger(self, n: int) -> int:
+#         total_sum = (1 + n) * n // 2
+#         for x in range(1, n+1):
+#             a = x * (1 + x) - x 
+#             if a == total_sum:
+#                 return x
+#             if a > total_sum:
+#                 return -1
+
+
