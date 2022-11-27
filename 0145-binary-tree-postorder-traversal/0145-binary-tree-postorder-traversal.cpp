36bc3aa93de2bdf1689198cf462c5d8bfac79b7d
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2022-11-27 19:04:12 +0530

    Time: 5 ms (26.74%), Space: 8.6 MB (37.49%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -1,25 +1,44 @@
-class Solution {
+//Recursive Approach
+
+class Solution { 
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int>answer;
-        stack<TreeNode*> s;
-        TreeNode* currentNode=NULL;
-        if(root==NULL)
-            return answer;
-        s.push(root);
-        while(!s.empty()){
-             currentNode= s.top();
-            s.pop();
-            answer.push_back(currentNode->val);
-            if(currentNode->left!=NULL)
-                s.push(currentNode->left);
-            
-            if(currentNode->right!=NULL)
-                s.push(currentNode->right);
-        }
-        reverse(answer.begin(),answer.end());
-        return answer;
-        
-        
+        postorder(root);
+        return v;
+    }
+    vector<int> v;
+    void postorder(TreeNode* root)
+    {
+        if(root==NULL)return;
+        postorder(root->left);
+        postorder(root->right);
+        v.push_back(root->val);
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int>answer;
+//         stack<TreeNode*> s;
+//         TreeNode* currentNode=NULL;
+//         if(root==NULL)
+//             return answer;
+//         s.push(root);
+//         while(!s.empty()){
+//              currentNode= s.top();
+//             s.pop();
+//             answer.push_back(currentNode->val);
+//             if(currentNode->left!=NULL)
+//                 s.push(currentNode->left);
+            
+//             if(currentNode->right!=NULL)
+//                 s.push(currentNode->right);
+//         }
+//         reverse(answer.begin(),answer.end());
+//         return answer;
+        
+        
+//     }
+// };
