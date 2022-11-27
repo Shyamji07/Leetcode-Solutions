84b9b2f5800cf5ff8ea7968bd911b92336c18cee
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2022-11-27 14:43:04 +0530

    Time: 3 ms (90.91%), Space: 6 MB (45.45%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,14 +1,35 @@
+//Binary Search
+
+
+// class Solution {
+// public:
+//     int pivotInteger(int n) {
+//     int l = 1, r = n, sum = n * (n + 1) / 2;
+//     while (l < r) {
+//         int m = (l + r) / 2;
+//         if (m * m - sum < 0)
+//             l = m + 1;
+//         else
+//             r = m;
+//     }
+//     return l * l - sum == 0 ? l : -1;
+// }
+// };
+
+
+// Pre-Computation DP
+
+int dp[1001] = { 0, 0 };
 class Solution {
 public:
-    int pivotInteger(int n) {
-    int l = 1, r = n, sum = n * (n + 1) / 2;
-    while (l < r) {
-        int m = (l + r) / 2;
-        if (m * m - sum < 0)
-            l = m + 1;
-        else
-            r = m;
-    }
-    return l * l - sum == 0 ? l : -1;
+int pivotInteger(int n) {
+    if (dp[1] == 0)
+       for (int i = 1, j = 1; i <= 1000; ++i) {
+           int sum = i * (i + 1) / 2;
+           while (j * j < sum)
+               ++j;
+           dp[i] = j * j == sum ? j : -1;
+       }
+    return dp[n];
 }
 };
