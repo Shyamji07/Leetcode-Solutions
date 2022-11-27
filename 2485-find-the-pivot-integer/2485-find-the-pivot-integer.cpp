3eafe444cbfd412b6a4e24305de34fe0c27a71b1
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2022-11-27 14:44:40 +0530

    Time: 0 ms (100.00%), Space: 6 MB (45.45%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,32 +1,32 @@
 //Binary Search
 
-// class Solution {
-// public:
-//     int pivotInteger(int n) {
-//     int l = 1, r = n, sum = n * (n + 1) / 2;
-//     while (l < r) {
-//         int m = (l + r) / 2;
-//         if (m * m - sum < 0)
-//             l = m + 1;
-//         else
-//             r = m;
-//     }
-//     return l * l - sum == 0 ? l : -1;
-// }
-// };
+class Solution {
+public:
+    int pivotInteger(int n) {
+    int l = 1, r = n, sum = n * (n + 1) / 2;
+    while (l < r) {
+        int m = (l + r) / 2;
+        if (m * m - sum < 0)
+            l = m + 1;
+        else
+            r = m;
+    }
+    return l * l - sum == 0 ? l : -1;
+}
+};
 
 
 // Naive Approach
 
-class Solution {
-public:
-    int pivotInteger(int n) {
-        for(int i=1;i<=n;i++){
-            if((n*(n+1))==(2*i*i) )return i;
-        }
-        return -1;
-    }
-};
+// class Solution {
+// public:
+//     int pivotInteger(int n) {
+//         for(int i=1;i<=n;i++){
+//             if((n*(n+1))==(2*i*i) )return i;
+//         }
+//         return -1;
+//     }
+// };
 
 // Pre-Computation DP
 
