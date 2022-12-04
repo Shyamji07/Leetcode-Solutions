7834feaba3640cd0bea465533fe48c24e29df54a
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2022-12-04 16:27:08 +0530

    Time: 147 ms (70.69%), Space: 57.2 MB (62.07%) - LeetHub

---
+++ 2256-minimum-average-difference/2256-minimum-average-difference.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumAverageDifference = function(nums) {
+       let ans=0,sum=0;
+       let mini=Infinity;
+       let n=nums.length;
+       let curr=0;
+        for(let i=0;i<n;i++)sum+=nums[i];
+        for(let i=0;i<n-1;i++){
+           let temp=(Math.abs((parseInt)((nums[i]+curr)/(i+1))-(parseInt)((sum-nums[i]-curr)/(n-i-1))));
+            if(temp<mini)mini=temp,ans=i;
+            curr+=nums[i];
+        }
+        let temp=Math.floor(sum/n);
+        if(temp<mini)mini=temp,ans=n-1;
+        return ans;
+};
