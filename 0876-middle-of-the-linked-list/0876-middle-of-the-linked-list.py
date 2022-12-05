66f7e6ffc424e69e8128995b4e37c6f4bbb76684
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2022-12-05 22:40:46 +0530

    Time: 57 ms (47.78%), Space: 13.8 MB (96.17%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -1,20 +1,12 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-var middleNode = function(head) {
-    let slow = head;
-    let fast = head;
-        while (fast && fast.next) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
-};
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+       fast = slow = head
+       while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+       return slow 
