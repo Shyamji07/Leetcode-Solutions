d11eaec243e90456b922953633301f00d3891d95
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-01-01 17:59:47 +0530

    Time: 23 ms (40.00%), Space: 9 MB (40.00%) - LeetHub

--- 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.cpp
+++ 2522-partition-string-into-substrings-with-values-at-most-k/2522-partition-string-into-substrings-with-values-at-most-k.cpp
@@ -1,16 +1,28 @@
+// class Solution {
+// public:
+//     int minimumPartition(string s, int k) {
+//     long long n = 0, res = 1;
+//     for (int i = 0; i < s.size(); ++i) {
+//         if (n * 10 + (s[i] - '0') > k) {
+//             ++res;
+//             n = 0;
+//         }
+//         n = n * 10 + (s[i] - '0');
+//         if (n > k)
+//             return -1;
+//     }
+//     return res;
+// }
+// };
 class Solution {
 public:
     int minimumPartition(string s, int k) {
-    long long n = 0, res = 1;
-    for (int i = 0; i < s.size(); ++i) {
-        if (n * 10 + (s[i] - '0') > k) {
-            ++res;
-            n = 0;
+        int n = to_string(k).length(), res = 0;
+        for (int i = 0; i < s.length();) {
+            int used = stoi(s.substr(i, n)) <= k ? n : n - 1;
+            if (used <= 0) return -1;
+            res++, i += used;
         }
-        n = n * 10 + (s[i] - '0');
-        if (n > k)
-            return -1;
+        return res;
     }
-    return res;
-}
 };
