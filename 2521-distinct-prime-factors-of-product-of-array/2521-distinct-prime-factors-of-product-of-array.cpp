c2a2dbe08488caaf7a4bb0dc58330c4e8e06ccfc
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-01-01 18:04:29 +0530

    Time: 40 ms (100.00%), Space: 18.9 MB (45.45%) - LeetHub

--- 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -1,23 +1,37 @@
-int ps[11] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
-// We only need to know primes up to sqrt(1000).
+// int ps[11] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
+// // We only need to know primes up to sqrt(1000).
 
-// If, after dividing n by all these primes, n is still greater than 1, then what remains is a large prime.
+// // If, after dividing n by all these primes, n is still greater than 1, then what remains is a large prime.
 
+
+// class Solution {
+// public:
+// int distinctPrimeFactors(vector<int>& nums) {
+//     unordered_set<int> primes;
+//     for (int n : nums) {
+//         for (int p : ps)
+//             if (n % p == 0) {
+//                 primes.insert(p);
+//                 while (n % p == 0)
+//                     n /= p;
+//             }
+//         if (n != 1)
+//             primes.insert(n); // large prime.
+//     }
+//     return primes.size();
+// }
+// };
 
 class Solution {
 public:
-int distinctPrimeFactors(vector<int>& nums) {
-    unordered_set<int> primes;
-    for (int n : nums) {
-        for (int p : ps)
-            if (n % p == 0) {
-                primes.insert(p);
-                while (n % p == 0)
-                    n /= p;
+    int distinctPrimeFactors(vector<int>& nums) {
+        set<int> g;
+        for(auto t:nums) {
+            for(int i=2;i*i<=t;++i) if(t%i==0) {
+                g.insert(i); while(t%i==0) t/=i;
             }
-        if (n != 1)
-            primes.insert(n); // large prime.
+            if(t>1) g.insert(t);
+        }
+        return g.size();
     }
-    return primes.size();
-}
 };
