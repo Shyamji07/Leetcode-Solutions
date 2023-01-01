a97529b575ff4fd256976f297e52efc8cf2633d5
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-01-01 18:03:40 +0530

    Time: 48 ms (90.91%), Space: 18.8 MB (45.45%) - LeetHub

--- 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -1,4 +1,9 @@
 int ps[11] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
+// We only need to know primes up to sqrt(1000).
+
+// If, after dividing n by all these primes, n is still greater than 1, then what remains is a large prime.
+
+
 class Solution {
 public:
 int distinctPrimeFactors(vector<int>& nums) {
