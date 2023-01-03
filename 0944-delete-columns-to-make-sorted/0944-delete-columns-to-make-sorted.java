1d5433deeceacbc485465f3871d9c35a5456ca12
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-01-03 07:20:46 +0530

    Time: 9 ms (84.49%), Space: 42.8 MB (67.42%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -1,13 +1,12 @@
 class Solution {
-public:
-    int minDeletionSize(vector<string>& strs) {
+    public int minDeletionSize(String[] strs) {
     int remove = 0;
-    for(int i=0 ; i<strs[0].size();++i)
-        for(int j=0;j<strs.size()-1;++j)
-            if(strs[j][i] > strs[j+1][i]){
+    for(int i=0 ; i<strs[0].length();i++)
+        for(int j=0;j<strs.length-1;j++)
+            if(strs[j].charAt(i) > strs[j+1].charAt(i)){
                 ++remove;
                 break;
             }
     return remove;
     }
-};
+}
