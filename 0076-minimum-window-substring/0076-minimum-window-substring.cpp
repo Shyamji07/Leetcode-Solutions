fc5f9707e6b43e52dce87d6a560b8c661b211b0b
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-01-08 20:05:47 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (99.61%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int m[256] = { 0 };
+     int ans = INT_MAX; // length of ans
+    int start = 0; // starting index of ans
+    int count = 0;
+   
+    // creating map
+    for (int i = 0; i < t.length(); i++) {
+        if (m[t[i]] == 0)
+            count++;
+        m[t[i]]++;
+    }
+ 
+    // References of Window
+    int i = 0;
+    int j = 0;
+ 
+    // Traversing the window
+    while (j < s.length()) {
+        // Calculations
+        m[s[j]]--;
+        if (m[s[j]] == 0)
+            count--;
+ 
+        if (count == 0) {
+            while (count == 0) {
+                if (ans > j - i + 1) {
+                    ans = j - i + 1;
+                    start = i;
+                }
+                m[s[i]]++;
+                if (m[s[i]] > 0)
+                    count++;
+ 
+                i++;
+            }
+        }
+        j++;
+    }
+ 
+    if (ans != INT_MAX)
+        return s.substr(start, ans);
+    else
+        return "";
+    }
+};
