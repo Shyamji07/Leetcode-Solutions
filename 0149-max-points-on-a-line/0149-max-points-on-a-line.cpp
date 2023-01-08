9a64873c50292ca98a1bfd78d6fb39056a31cc29
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-01-08 16:47:10 +0530

    Time: 47 ms (81.11%), Space: 20.3 MB (16.94%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,43 +1,22 @@
 class Solution {
 public:
-  
-    void simplify(int & x,int &y)
-    {
-        int d; 
-        d = __gcd(x, y); 
-
-        x = x / d; 
-        y = y / d; 
-    }
     int maxPoints(vector<vector<int>>& points) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(NULL);
-        int omax=0;
-        for(int i=0;i<points.size();i++)
-        {
-            unordered_map<string,int>map;
-            int cmax=0;
-            int count=1;
-            for(int j=i+1;j<points.size();j++)
-            {
-                
-                // if(points[i][0]==points[j][0]&&points[i][1]==points[j][1])
-                // {
-                //     count++;
-                // }
-                // else
-                {
-                    int nr=(points[j][1]-points[i][1]);
-                    int dr=(points[i][0]-points[j][0]);
-                    simplify(nr,dr);
-                    string fin=to_string(nr)+"#"+to_string(dr);
-                    map[fin]++;
-                    cmax=max(cmax,map[fin]);
-                }
-                
-            }
-            omax=max(omax,cmax+count);
+        int n = points.size();
+        if (n == 1) {
+            return 1;
         }
-        return omax;
+        int result = 2;
+        for (int i = 0; i < n; i++) {
+            unordered_map<double, int> cnt;
+            for (int j = 0; j < n; j++) {
+                if (j != i) {
+                    cnt[atan2(points[j][1] - points[i][1], points[j][0] - points[i][0])]++;
+                }
+            }
+            for (auto [h, count] : cnt) {
+                result = max(result, count + 1);
+            }
+        }
+        return result;
     }
 };
