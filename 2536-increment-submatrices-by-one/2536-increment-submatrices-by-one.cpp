106c71762c5cdc324d215ed1d0e9dd55e787602b
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-01-15 15:58:43 +0530

    Time: 338 ms (80.00%), Space: 81.9 MB (40.00%) - LeetHub

--- 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
+++ 2536-increment-submatrices-by-one/2536-increment-submatrices-by-one.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-        vector<vector<int>> mat(n, vector<int>(n, 0));
         
-        for (auto q : queries) {
-            int r1 = q[0], c1 = q[1], r2 = q[2], c2 = q[3];
+        vector<vector<int>> temp(n, vector<int>(n,0));
+        
+        for(int i=0; i<queries.size(); i++){
+            int toprow= queries[i][0];
+            int leftcol= queries[i][1];
+            int bottomrow= queries[i][2];
+            int rightcol= queries[i][3];
             
-            mat[r1][c1] += 1;
-            if(r2 + 1 < n && c2 + 1 < n) mat[r2 + 1][c2 + 1] += 1;
-            if(r2 + 1< n) mat[r2 + 1][c1] -= 1;
-            if(c2 + 1 < n) mat[r1][c2 + 1] -= 1;
+           
+            // prefix sum concept
+            // basically , for every row toprow -> bottomrow, ist column of each row ( temp[j][leftcol]) will be done += (+1)
+            // and the next column of the last column(except the last column, which we can ignore) of each row (i.e, temp[j][rightcol+1]) will be done += (-1)
+            // so that we get our desired sums while doing cumulative sum at last...
+            for(int j=toprow; j<=bottomrow; j++){
+                temp[j][leftcol] +=1;
+                if( rightcol<n-1)
+                    temp[j][rightcol+1] +=-1;
+            }
         }
         
-        for(int i=0; i<n; i++) {
-            for (int j=1; j<n; j++) {
-                mat[i][j] += mat[i][j - 1];
-            }
-        }
-        for(int i=1; i<n; i++) {
-            for (int j=0; j<n; j++) {
-                mat[i][j] += mat[i - 1][j];
-            }
-        }
-        return mat;
+        // for every row, calculate prefix sum 
+        for(int i=0; i<n; i++)
+            for(int j=1; j<n; j++)
+                temp[i][j]+= temp[i][j-1];
+    
+
+        return temp;
     }
 };
