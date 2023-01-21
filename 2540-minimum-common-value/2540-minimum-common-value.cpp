84897fcb348bfee57a759e81e1d77398e84c5844
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-01-21 23:17:00 +0530

    Time: 192 ms (50.00%), Space: 66.5 MB (5.56%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getCommon(vector<int>& arr1, vector<int>& arr2) {
+        set<int>st(begin(arr1),end(arr1));
+        int mini=INT_MAX;
+        for(auto a:arr2){
+            if(st.find(a)!=st.end())
+                mini=min(mini,a);
+        }
+        return mini==INT_MAX?-1:mini;
+    }
+};
