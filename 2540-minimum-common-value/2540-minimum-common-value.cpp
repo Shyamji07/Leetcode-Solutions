5e01b483c4968e89024d3410c01efcc0a79d08f6
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-01-21 23:18:32 +0530

    Time: 154 ms (66.67%), Space: 50.4 MB (33.33%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     int getCommon(vector<int>& arr1, vector<int>& arr2) {
-        unordered_set<int>st(begin(arr1),end(arr1));
-        int mini=INT_MAX;
-        for(auto a:arr2){
-            if(st.find(a)!=st.end())
-                mini=min(mini,a);
+        int i = 0, j = 0;
+        while(i < arr1.size() && j < arr2.size() ){
+            if(arr1[i] == arr2[j]) return arr1[i];
+            if(arr1[i] > arr2[j]) j++;
+            else i++;
         }
-        return mini==INT_MAX?-1:mini;
+        return -1;
     }
 };
+
+
+// class Solution {
+// public:
+//     int getCommon(vector<int>& arr1, vector<int>& arr2) {
+//         unordered_set<int>st(begin(arr1),end(arr1));
+//         int mini=INT_MAX;
+//         for(auto a:arr2){
+//             if(st.find(a)!=st.end())
+//                 mini=min(mini,a);
+//         }
+//         return mini==INT_MAX?-1:mini;
+//     }
+// };
