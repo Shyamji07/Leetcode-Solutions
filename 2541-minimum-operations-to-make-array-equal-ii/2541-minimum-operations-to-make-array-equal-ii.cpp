1586358269923e44c11880a4eb9b205093783763
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-01-21 23:21:43 +0530

    Time: 202 ms (30.00%), Space: 72.3 MB (30.00%) - LeetHub

--- 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.cpp
+++ 2541-minimum-operations-to-make-array-equal-ii/2541-minimum-operations-to-make-array-equal-ii.cpp
@@ -1,19 +1,34 @@
-class Solution:
-    def minOperations(self, arr1: List[int], arr2: List[int], k: int) -> int:
-        abss = [0]*len(arr1)
-        if arr1==arr2:
-            return 0
-        for i in range(len(arr1)):
-            abss[i] = arr1[i]-arr2[i]
-
-        if sum(abss)!=0:
-            return -1
-        ans = 0
-        for i in abss:
-            if i>0:
-                ans+=i
-
-        if k!=0 and ans%k==0:
-            return ans//k
-        else:
-            return -1
+class Solution 
+{
+public:
+    long long minOperations(vector<int>& nums1, vector<int>& nums2, int k) 
+    {
+        if(k==0) //if k is 0 then we can do nothing
+        {
+            //if nums1 and nums2 are equal operations will be 0 
+            //otherwise it is impossible to make them equal
+            if(nums1==nums2) return 0; 
+            return -1;
+        }
+        
+        //p_diff stores positive difference between nums1[i] and nums2[i] 
+        //n_diff stores negative difference between nums1[i] and nums2[i] 
+        long long p_diff=0, n_diff=0; 
+        
+        for(int i=0; i<nums1.size(); i++)
+        {
+            if(nums1[i] >= nums2[i]) p_diff += nums1[i]-nums2[i];
+            else n_diff += nums2[i]-nums1[i];
+        }
+        
+        long long ans=-1;
+        //if both difference(p_diff,n_diff) are equal, we can make nums1 equals to nums2
+        //we also need to check whether we can make them equal by using k
+        if((p_diff-n_diff==0) && (p_diff%k)==0)
+        {
+             ans = (p_diff/k);
+        }
+        return ans;
+        
+    }
+};
