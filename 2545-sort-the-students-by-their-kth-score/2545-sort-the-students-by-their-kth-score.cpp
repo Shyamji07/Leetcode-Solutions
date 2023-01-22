26bee49948109c721ea846c37d029b06bd736d8a
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-01-22 23:25:00 +0530

    Time: 86 ms (69.23%), Space: 27.2 MB (84.62%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
@@ -1,19 +1,32 @@
+int r;
+bool comp(vector<int>& a, vector<int>& b)
+{   
+    return (a[r]>b[r]); 
+}
 class Solution {
 public:
-    vector<vector<int>> sortTheStudents(vector<vector<int>>& arr, int k) {
-        bool flag=true;
-        for (int i = 0; i< arr.size();i++){
-            if(flag){
-                flag = false;
-                for(int jj=0;jj<arr.size()-1;jj++){
-                    if(arr[jj][k] < arr[jj+1][k]){
-                        arr[jj].swap(arr[jj+1]);
-                        flag = true;
-                    }
-                }
-            }
-            else break;
-        }
-        return arr;
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        r=k;
+        sort(score.begin(),score.end(),comp);
+        return score;
     }
 };
+// class Solution {
+// public:
+//     vector<vector<int>> sortTheStudents(vector<vector<int>>& arr, int k) {
+//         bool flag=true;
+//         for (int i = 0; i< arr.size();i++){
+//             if(flag){
+//                 flag = false;
+//                 for(int jj=0;jj<arr.size()-1;jj++){
+//                     if(arr[jj][k] < arr[jj+1][k]){
+//                         arr[jj].swap(arr[jj+1]);
+//                         flag = true;
+//                     }
+//                 }
+//             }
+//             else break;
+//         }
+//         return arr;
+//     }
+// };
