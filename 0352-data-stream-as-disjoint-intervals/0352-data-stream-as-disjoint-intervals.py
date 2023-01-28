a49d957bd35ad312f8e41655a7166510327e3600
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-01-28 23:07:48 +0530

    Time: 610 ms (21.07%), Space: 18.9 MB (99.00%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.py
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.py
@@ -1,52 +1,27 @@
-class SummaryRanges {
-public:
-    SummaryRanges() {
-        
-    }
-    void addNum(int value) {
-        auto it = mp.lower_bound(value);
-        bool flag = false;
-        if(it != mp.begin()) {
-            auto previous = it;
-            --previous;
-            if(previous->second + 1 >= value) {
-                flag = true;
-                previous->second = max(previous->second, value);
-            }
-        }
+class SummaryRanges:
 
-        if(it != mp.end()) {
-            if(it->first - 1 <= value) {
-                if(flag) {
-                    auto previous = it;
-                    --previous;
-                    if(previous->second >= it->first - 1) {
-                        previous->second = max(previous->second, it->second);
-                        mp.erase(it);
-                    }
-                } else {
-                    flag = true;
-                    if(it->first != value) {
-                        pair<int, int> p = *it;
-                        p.first = min(p.first, value);
-                        it = mp.insert(it, p);
-                        ++it;
-                        if(it != mp.end())
-                            mp.erase(it);
-                    }
-                }
-            }
-        }
-        if(!flag)
-            mp.insert(it, {value, value});
-    }
+    def __init__(self):
+        self.nums = set()
 
-    vector<vector<int>> getIntervals() {
-        vector<vector<int>> ans;
-        for(auto const & p : mp)
-            ans.push_back({p.first, p.second});
-        return ans;
-    }
+    def addNum(self, value: int) -> None:
+        self.nums.add(value)
 
-    map<int, int> mp;
-};
+    def getIntervals(self) -> List[List[int]]:
+        arr = []
+        vis = set()
+        for num in self.nums:
+            if num in vis:
+                continue
+
+            left = num
+            while left - 1 in self.nums:
+                left -= 1
+                vis.add(left)
+
+            right = num
+            while right + 1 in self.nums:
+                right += 1
+                vis.add(right)
+
+            arr.append([left, right])
+        return sorted(arr)
