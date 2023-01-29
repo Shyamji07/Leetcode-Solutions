1b600e81ac15bb2cdc9aea377763c015bf988068
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-01-29 17:47:56 +0530

    Time: 866 ms (8.33%), Space: 20.2 MB (25.00%) - LeetHub

--- 2552-count-increasing-quadruplets/2552-count-increasing-quadruplets.cpp
+++ 2552-count-increasing-quadruplets/2552-count-increasing-quadruplets.cpp
@@ -1,25 +1,39 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ll long long
+#define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
+
 class Solution {
 public:
     long long countQuadruplets(vector<int>& nums) {
-        int n = nums.size();
-
-        int lessthan[n+1][n+1];
-        for(int i = 0; i <= n; i++) lessthan[i][0] = 0;        
-        for(int i = 1; i <= n; i++) 
-            for(int j = 0; j <= n; j++) 
-                lessthan[j][i] = lessthan[j][i-1] + (nums[i-1] < j);
         
-        long long re = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                if(nums[i] > nums[j]){
-                    
-                    long long g = n-nums[i]-(j - lessthan[nums[i]][j+1]);
-                    long long l = lessthan[nums[j]][i+1];
-                    re += g*l;
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        
+        int n= nums.size();
+
+        ordered_set st2;
+        ll ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int cnt=0;
+            for(int j=n-1;j>i;j--)
+            {
+                if(nums[j]< nums[i])
+                {
+                   int x= st2.order_of_key(nums[j]);
+                   int y= cnt;
+                   ans+= x* y;
                 }
+                else if(nums[j] > nums[i]) cnt++;
             }
+            st2.insert(nums[i]);
+            
         }
-        return re;
+        
+        return ans;
+        
     }
 };
