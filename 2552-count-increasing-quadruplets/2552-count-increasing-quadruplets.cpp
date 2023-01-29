7ee49ed4c0ce300d1810f6f53246aeab6603ba23
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-01-29 17:45:52 +0530

    Time: 621 ms (8.33%), Space: 81 MB (8.33%) - LeetHub

--- 2552-count-increasing-quadruplets/2552-count-increasing-quadruplets.cpp
+++ 2552-count-increasing-quadruplets/2552-count-increasing-quadruplets.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    long long countQuadruplets(vector<int>& a) {
-        long long res = 0;
-        int n = a.size();
-        vector<vector<int>> left(n, vector<int>(n+1, 0));
-        vector<vector<int>> right(n, vector<int>(n+1, 0));
-        for (int i = 1; i < n; ++i) {
-            // new array will based on the old array
-            left[i] = left[i-1];
-            // update all the elements greater than a[i-1]
-            for (int j = a[i-1] + 1; j <= n; ++j)
-                left[i][j]++;
-        }
-        for (int i = n-2; i >= 0; --i) {
-            right[i] = right[i+1];
-            for (int j = 0; j < a[i+1]; ++j)
-                right[i][j]++;
-        }
-        for (int i = 0; i < n; ++i) {
-            for (int j = n-1; j > i; --j) {
-                if (a[i] <= a[j]) continue;
-                // left[i][a[j]] means the count of "until i, < a[j]"
-                // right[j][a[i]] means the count of "unitl j, > a[i]"
-                res += left[i][a[j]] * right[j][a[i]];
+    long long countQuadruplets(vector<int>& nums) {
+        int n = nums.size();
+
+        int lessthan[n+1][n+1];
+        for(int i = 0; i <= n; i++) lessthan[i][0] = 0;        
+        for(int i = 1; i <= n; i++) 
+            for(int j = 0; j <= n; j++) 
+                lessthan[j][i] = lessthan[j][i-1] + (nums[i-1] < j);
+        
+        long long re = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                if(nums[i] > nums[j]){
+                    
+                    long long g = n-nums[i]-(j - lessthan[nums[i]][j+1]);
+                    long long l = lessthan[nums[j]][i+1];
+                    re += g*l;
+                }
             }
         }
-        return res;
+        return re;
     }
 };
