285c8bfd9c65610dfeff2a766165563d1c70bb7e
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-02-05 15:48:11 +0530

    Time: 180 ms (100.00%), Space: 57 MB (100.00%) - LeetHub

--- 2560-house-robber-iv/2560-house-robber-iv.cpp
+++ 2560-house-robber-iv/2560-house-robber-iv.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
-    bool isPossible(vector<int> &a, int k, int req){
-        vector<int> dp(a.size());
-        for(int i=0;i<a.size();i++){
-            dp[i]=-1;
+   int minCapability(vector<int>& A, int k) {
+        int left = 1, right = 1e9, n = A.size();
+        while (left < right) {
+            int mid = (left + right) / 2, take = 0;
+            for (int i = 0; i < n; ++i)
+                if (A[i] <= mid) {
+                    take += 1;
+                    i++;
+                }
+            if (take >= k)
+                right = mid;
+            else
+                left = mid + 1;
         }
-        int maxi=help(a,k,0,dp);
-        return maxi>=req;
-    }
-    int help(vector<int> &arr, int k, int i, vector<int> &v){
-        if(i>=arr.size())return 0;
-        if(v[i]!=-1)return v[i];
-        int temp=help(arr,k,i+1,v);
-        if(arr[i]<=k){
-            return v[i]= max(help(arr,k,i+2,v)+1,temp);
-        }
-        return v[i]= temp;
-    }
-    
-    int minCapability(vector<int>& nums, int k) {
-        int l=0, r=1e9;
-        while(l<r-1){
-            int mid=l+(r-l)/2;
-            if(isPossible(nums,mid,k))
-                r=mid;
-            else l=mid;
-   
-        }
-        return r;
+        return left; 
     }
 };
