4800492ba4adecbcea4ab835fbb778733e438073
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-02-09 08:19:11 +0530

    Time: 2524 ms (6.50%), Space: 426.7 MB (5.69%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.cpp
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -5,10 +5,10 @@ public:
         vector<unordered_set<string>> arr(26);
         for (auto s : ideas) 
             arr[s[0] - 'a'].insert(s.substr(1));
-        unordered_set<string> st;
+        
         for (int i = 0; i < 25; i++) {
             for (int j = i + 1; j < 26; j++) {
-                st.clear();
+                unordered_set<string> st;
                 st.insert(arr[i].begin(), arr[i].end());
                 st.insert(arr[j].begin(), arr[j].end());
                 count += (arr[i].size() - st.size()) * (arr[j].size() - st.size());
