c568229ba920182dc4e10ccd867584d1af06fdd0
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-02-13 07:00:51 +0530

    Time: 59 ms (93.15%), Space: 41.2 MB (95.16%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -1,3 +1,8 @@
-class Solution:
-    def countOdds(self, low: int, high: int) -> int:
-        return ((high - low) // 2) + ((low | high) & 1);
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+    return Math.floor((high - low) / 2) + ((low | high) & 1);
+};
