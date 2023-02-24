a7a97d7ba1336636110cbf794f4d93f373364903
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-02-24 07:15:24 +0530

    Time: 191 ms (99.15%), Space: 58.9 MB (49.15%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int minimumDeviation(vector<int>& nums) {
-       set <int>  s;
-        for(int i = 0; i<nums.size() ; ++i)
-        {if(nums[i] % 2 == 0)
-                s.insert(nums[i]);
-            else
-                s.insert(nums[i] * 2);
+        vector<int> odds = nums;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            while(odds[i] % 2 == 0) odds[i] >>= 1;
         }
-        int diff = *s.rbegin() - *s.begin();
-        while(*s.rbegin() % 2 == 0)
-        {  int x = *s.rbegin();
-            s.erase(x);
-            s.insert(x/2);
-            diff = min(diff, *s.rbegin() - *s.begin());
+        int max_odd = *max_element(odds.begin(), odds.end());
+        vector<int> v;
+        for(auto x: nums){
+            if(x % 2) v.push_back(x);
+            else{
+                do{
+                    x >>= 1;
+                } while(x > max_odd);
+                v.push_back(x);
+            }
         }
-        return diff;
+        sort(v.begin(), v.end());
+        int ans = v[n - 1] - v[0];
+        for(int i = 0; i < n - 1; i++){
+            ans = min(ans, max(v[n - 1], 2 * v[i]) - min(v[i + 1], 2 * v[0]));
+        }
+        return ans;
     }
 };
