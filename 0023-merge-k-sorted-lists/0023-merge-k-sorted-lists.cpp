b1e92aea14d4604af8045f2b7a026dd15b975a14
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-03-12 17:17:46 +0530

    Time: 21 ms (86.20%), Space: 13.5 MB (46.15%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    ListNode* merge(ListNode* l1, ListNode* l2){
+        if(!l1)return l2;
+        if(!l2)return l1;
+            if(l1->val<=l2->val){
+                 l1->next=merge(l1->next,l2);
+                return l1;
+            }
+            else {
+                l2->next=merge(l1,l2->next);
+                return l2;
+        }
+        return l1;
+    }
+    ListNode* divide(vector<ListNode*>& lists,int l,int r){
+        if(l>r)return NULL;
+        if(l==r)return lists[l];
+        int mid=l+(r-l)/2;
+        ListNode *left=divide(lists,l,mid);
+        ListNode * right=divide(lists,mid+1,r);
+        lists[l]=merge(left, right);
+        return lists[l];
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0)return nullptr;
+        return divide(lists,0,lists.size()-1);
+    }
+};
