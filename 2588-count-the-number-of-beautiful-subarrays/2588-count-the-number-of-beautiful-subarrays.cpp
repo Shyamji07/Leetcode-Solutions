75f205d2a2e9b8b859371749fd3dc6bd295cbfc3
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-03-12 17:41:20 +0530

    Time: 1234 ms (100.00%), Space: 37.2 MB (100.00%) - LeetHub

--- 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.cpp
+++ 2588-count-the-number-of-beautiful-subarrays/2588-count-the-number-of-beautiful-subarrays.cpp
@@ -1,10 +1,10 @@
-class Solution {
-public:
-    long long beautifulSubarrays(vector<int>& nums) {
-        unordered_map<int, int> dp{{0, 1}};
-        long long res = 0, pre = 0;
-        for (int a : nums)
-            res += dp[pre ^= a]++;
-        return res;
-    }
-};
+class Solution:
+    def beautifulSubarrays(self, nums: List[int]) -> int:
+        dp = Counter({0: 1})
+        res = pre = 0
+        for a in nums:
+            pre ^= a
+            res += dp[pre]
+            dp[pre] += 1
+        return res
+        
