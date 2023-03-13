28326d46eaff1a37db5a30a0ec2092fa89d8e2d8
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-03-13 17:38:02 +0530

    Time: 64 ms (88.10%), Space: 44.7 MB (41.93%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+ var is=( lr, rr)=>{
+      if(!lr && !rr)return true;
+      if(!lr || !rr ||lr.val!=rr.val)return false;
+      return is(lr.left,rr.right)&&is(lr.right,rr.left);
+  }
+
+var isSymmetric = function(root) {
+    if(!root)return true;
+      return is(root.left,root.right);
+};
