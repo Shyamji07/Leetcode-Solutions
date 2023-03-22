f0d143b6ff39495eaee62b730e0883a285318868
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-03-22 22:20:19 +0530

    Time: 530 ms (54.77%), Space: 140.3 MB (27.08%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,5 +1,47 @@
+// class Solution {
+// public:
+//     int minScore(int n, vector<vector<int>>& roads) {
+//         int ans = INT_MAX;
+//         vector<vector<pair<int, int>>> gr(n+1);
+//         for(auto edge : roads){ 
+//             gr[edge[0]].push_back({edge[1], edge[2]}); // u-> {v, dis}
+//             gr[edge[1]].push_back({edge[0], edge[2]}); // v-> {u, dis}
+//         }
+
+//         vector<int> vis(n+1, 0);
+//         queue<int> q;
+//         q.push(1); vis[1] = 1;
+//         while(!q.empty()){
+//             auto node = q.front(); q.pop();
+//             for(auto& [v, dis] : gr[node]){
+//                 ans = min(ans, dis);
+//                 if(vis[v]==0){
+//                     vis[v] = 1;
+//                     q.push(v);
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+
 class Solution {
 public:
+    int dfs(int node,int& ans, vector<vector<pair<int, int>>>& gr, vector<int>& vis){
+        vis[node] = 1;
+        for(auto& [v, dis] : gr[node]){
+            ans = min(ans, dis);
+            if(vis[v]==0){
+                vis[v] = 1;
+                ans = min(ans, dfs(v, ans, gr, vis));
+            }
+        }
+        return ans;
+    }
+
     int minScore(int n, vector<vector<int>>& roads) {
         int ans = INT_MAX;
         vector<vector<pair<int, int>>> gr(n+1);
@@ -9,18 +51,7 @@ public:
         }
 
         vector<int> vis(n+1, 0);
-        queue<int> q;
-        q.push(1); vis[1] = 1;
-        while(!q.empty()){
-            auto node = q.front(); q.pop();
-            for(auto& [v, dis] : gr[node]){
-                ans = min(ans, dis);
-                if(vis[v]==0){
-                    vis[v] = 1;
-                    q.push(v);
-                }
-            }
-        }
+        dfs(1, ans, gr, vis);
 
         return ans;
     }
