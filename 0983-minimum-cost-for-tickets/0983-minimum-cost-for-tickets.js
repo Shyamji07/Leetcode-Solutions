1f1af4561019d6bcca1abfb837521513fcad7a18
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-03-28 07:24:14 +0530

    Time: 70 ms (48.94%), Space: 42.8 MB (46.10%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.js
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.js
@@ -1,11 +1,22 @@
-class Solution:
-    def mincostTickets(self, days, costs):
+var mincostTickets = function(days, costs) {
+        const [ _1day, _7day, _30day] = [0, 1, 2];
+        let travelDays = new Set(days);
+        let lastTravelDay = days[days.length-1];
     
-        dp = [0] * 366
-        travel_days = set(days)
-        for i in range(1, 366):
-            if i not in travel_days:
-                dp[i] = dp[i-1]
-            else:
-                dp[i] = min(dp[i-1] + costs[0], dp[max(0, i-7)] + costs[1], dp[max(0, i-30)] + costs[2])
-        return dp[365]
+    dpCost = new Array( lastTravelDay+1 ).fill(0);
+    
+    for( let day_i=1; day_i <= lastTravelDay ; day_i++ ){
+        if( travelDays.has(day_i) == false ){
+            dpCost[ day_i ] = dpCost[ day_i - 1 ];
+            
+        }
+        else{
+            dpCost[ day_i ] = Math.min( dpCost[ day_i - 1 ] + costs[ _1day ],
+                                        dpCost[ Math.max(day_i - 7, 0) ] + costs[ _7day ], 
+                                        dpCost[ Math.max(day_i - 30, 0) ] + costs[_30day]  );
+        }
+    }
+
+    return dpCost[lastTravelDay];
+    
+};
