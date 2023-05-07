557fca1a4072da14c1cc23635930c684a238ea90
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-07 09:13:48 +0530

    Time: 321 ms (52.08%), Space: 123.3 MB (20.83%) - LeetHub

--- 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
+++ 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
@@ -1,13 +1,34 @@
-class Solution:
-    def longestObstacleCourseAtEachPosition(self, obstacles: List[int]) -> List[int]:
-        n = len(obstacles)
-        res = n*[1]
-        v = []
-        for i in range(n):
-            tr = bisect_right(v, obstacles[i])
-            if tr == len(v):
-                v.append(obstacles[i])
-            else:
-                v[tr] = obstacles[i]
-            res[i] = tr + 1
-        return res
+class Solution {
+public:
+    // vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
+    //     int n = obstacles.size();
+    //     vector<int> res(n, 1);
+    //     vector<int> v;
+    //     for(int i=0; i<n; i++) {
+    //         int tr = upper_bound(v.begin(),v.end(),obstacles[i]) - v.begin();
+    //         if(tr == v.size()) v.push_back(obstacles[i]);
+    //         else v[tr] = obstacles[i];
+    //         res[i] = tr + 1;
+    //     }
+    //     return res;
+    // }
+    
+    vector<int> longestObstacleCourseAtEachPosition(vector<int>& A) {
+        vector<int> res, mono;
+        for (int a : A) {
+            int l = 0, r = mono.size();
+            while (l < r) {
+                int m = (l + r) / 2;
+                if (mono[m] <= a)
+                    l = m + 1;
+                else
+                    r = m;
+            }
+            res.push_back(l + 1);
+            if (mono.size() == l)
+                mono.push_back(a);
+            mono[l] = a;
+        }
+        return res;
+    }
+};
