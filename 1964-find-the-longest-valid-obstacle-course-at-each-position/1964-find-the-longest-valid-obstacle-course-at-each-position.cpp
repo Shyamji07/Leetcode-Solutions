d5e58c4b2afe96faa11e73d7178110e07609b730
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-07 09:10:08 +0530

    Time: 1456 ms (100.00%), Space: 33.8 MB (15.15%) - LeetHub

--- 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
+++ 1964-find-the-longest-valid-obstacle-course-at-each-position/1964-find-the-longest-valid-obstacle-course-at-each-position.cpp
@@ -1,15 +1,13 @@
-class Solution {
-public:
-    vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
-        int n = obstacles.size();
-        vector<int> res(n, 1);
-        vector<int> v;
-        for(int i=0; i<n; i++) {
-            int tr = upper_bound(v.begin(),v.end(),obstacles[i]) - v.begin();
-            if(tr == v.size()) v.push_back(obstacles[i]);
-            else v[tr] = obstacles[i];
-            res[i] = tr + 1;
-        }
-        return res;
-    }
-};
+class Solution:
+    def longestObstacleCourseAtEachPosition(self, obstacles: List[int]) -> List[int]:
+        n = len(obstacles)
+        res = n*[1]
+        v = []
+        for i in range(n):
+            tr = bisect_right(v, obstacles[i])
+            if tr == len(v):
+                v.append(obstacles[i])
+            else:
+                v[tr] = obstacles[i]
+            res[i] = tr + 1
+        return res
