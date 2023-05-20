3b5da9babfce4ce4c38871bdaac0eb92910df972
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-20 19:42:19 +0530

    Time: 346 ms (61.87%), Space: 259.7 MB (31.56%) - LeetHub

--- 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.cpp
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
-    bool doesValidArrayExist(vector<int>& A) {
-        return accumulate(A.begin(), A.end(), 0) % 2 == 0;
+    // bool doesValidArrayExist(vector<int>& A) {
+    //     return accumulate(A.begin(), A.end(), 0) % 2 == 0;
+    // }
+    bool doesValidArrayExist(vector<int>& derived) { 
+        return accumulate(begin(derived), end(derived), 0, bit_xor<>()) == 0;
     }
 };
