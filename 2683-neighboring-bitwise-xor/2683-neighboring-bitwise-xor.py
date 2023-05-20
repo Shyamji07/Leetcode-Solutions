77b5a224a5a20734f8cb791b3c25404181dd9db6
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-20 19:48:30 +0530

    Time: 2234 ms (85.48%), Space: 20.3 MB (14.84%) - LeetHub

--- 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.py
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.py
@@ -1,9 +1,4 @@
-class Solution {
-public:
-    // bool doesValidArrayExist(vector<int>& A) {
-    //     return accumulate(A.begin(), A.end(), 0) % 2 == 0;
-    // }
-    bool doesValidArrayExist(vector<int>& derived) { 
-        return accumulate(begin(derived), end(derived), 0, bit_xor<>()) == 0;
-    }
-};
+class Solution:
+    def doesValidArrayExist(self, derived: List[int]) -> bool:
+        return reduce(operator.xor, derived) == 0
+        
