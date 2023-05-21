f0fdadca73a544cb911a8cbe18560cce360e83fa
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-21 18:18:21 +0530

    Time: 1521 ms (75.00%), Space: 16.4 MB (25.00%) - LeetHub

--- 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
+++ 2698-find-the-punishment-number-of-an-integer/2698-find-the-punishment-number-of-an-integer.cpp
@@ -1,41 +1,22 @@
-// class Solution {
-// public:
-//      bool canSplit(int i, string s, int n, int sum) {
-//         if (i == s.length()) {
-//             if (sum == n){
-//                 return true;
-//             }
-//             return false;
-//           }
-//          for (int k = i; k < s.length(); k++) {
-//             if (canSplit(k + 1, s, n, sum + stoi(s.substr(i, k - i + 1)))){
-//               return true;
-//             }
-//           }
-//           return false;
-//     }
-//     int punishmentNumber(int n) {
-//       int ans = 0;
-//       for (int i = 1; i <= n; i++)
-//         if (canSplit(0, to_string(i * i), i, 0)) ans += i * i;
-//       return ans;
-//     }
-// };
-
-class Solution {
-public:
-  int punishmentNumber(int n) {
-    static vector<int> v{
-      1,   9,   10,  36,  45,  55,  82,  91,  99,  100, 235, 297, 369, 370,  379,
-      414, 657, 675, 703, 756, 792, 909, 918, 945, 964, 990, 991, 999, 1000,
-    };
-    int res = 0;
-    for (int i : v) {
-      if (i > n) {
-        break;
-      }
-      res += i * i;
-    }
-    return res;
-  }
-};
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        def check(x):
+            s = str(x * x)
+            n = len(s)
+            def dfs(i, tar):
+                if tar < 0:
+                    return False
+                if i >= n and tar == 0:
+                    return True
+                for j in range(i, n):
+                    if dfs(j+1, tar - int(s[i:j+1])):
+                        return True
+                return False
+            
+            return dfs(0, x)
+        
+        ans = 0
+        for i in range(1, n+1):
+            if check(i):
+                ans += i * i
+        return ans
