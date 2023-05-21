fb44fa49b452d2ad2584092e8a115f9ced5720c6
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-21 18:12:29 +0530

    Time: 40 ms (83.33%), Space: 15.2 MB (25.00%) - LeetHub

--- 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
     string makeSmallestPalindrome(string s) {
-        int i = 0;
-        int j = s.length() - 1;
-        while (i <= j) {
-            if (s[i] != s[j]) {
-                if (s[i] < s[j]) {
-                    s[j] = s[i];
-                } else {
-                    s[i] = s[j];
-                }
-            }
-            i++;
-            j--;
-        }
+        int n = s.size();
+        for (int i = 0; i < n; ++i)
+            s[i] = s[n - 1 - i] = min(s[i], s[n - i - 1]);
         return s;
     }
 };
