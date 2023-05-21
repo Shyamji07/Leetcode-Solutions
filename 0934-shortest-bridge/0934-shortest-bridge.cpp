c5b0dd29ecbfe7efd6217f4eeeb8e3d120a02ee6
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-21 14:41:48 +0530

    Time: 39 ms (97.59%), Space: 17.5 MB (97.44%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.cpp
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
    int n, m;
-    queue <int> q;
+    // queue <int> q;
+    int Q[10000];
+    int head = 0;
+    int tail = 0; 
     void dfs(vector <vector <int>> &a, int x, int y) {
         if (x >= 0 && x < n && y >= 0 && y < m && a[x][y] == 1) {
             a[x][y] = 2;
-            q.push(x * m + y);
+            Q[tail++] = x * m + y;
+            // q.push(x * m + y);
             dfs(a, x + 1, y);
             dfs(a, x - 1, y);
             dfs(a, x, y + 1);
@@ -13,16 +17,19 @@ public:
         }
     }
     int bfs(vector <vector <int>> &a) {
-        int d[5] = {0, 1, 0, -1, 0};
-        while(!q.empty()) { 
-            int zz = q.front(); q.pop();
+        int d[5] = {0, 1, 0, -1, 0}; 
+        // while(!q.empty()) {
+        while(head != tail){
+            // int zz = q.front(); q.pop();
+            int zz = Q[head++]; 
             int xx = zz / m, yy = zz % m;
             for (int k = 0; k < 4; k++) {
                 int x = xx + d[k], y = yy + d[k + 1];
                 if (x < 0 || y < 0 || x == n || y == n || a[x][y] > 1) continue;
                 if (a[x][y] == 1) return a[xx][yy] - 2;
                 a[x][y] = a[xx][yy] + 1;
-                q.push(x * m + y);
+                // q.push(x * m + y);
+                Q[tail++] = x * m + y;
             }
         } 
         return -1;
