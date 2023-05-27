0cfff0b886bdbe69c241ea6c4db13f36e925fe6e
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-28 00:32:55 +0530

    Time: 132 ms (68.18%), Space: 51.6 MB (95.45%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.js
@@ -0,0 +1,20 @@
+function stoneGameIII(stoneValue) {
+    const n = stoneValue.length;
+    const dp = new Array(4).fill(0);
+    for (let i = n - 1; i >= 0; i--) {
+        dp[i % 4] = stoneValue[i] - dp[(i + 1) % 4];
+        if (i + 2 <= n) {
+            dp[i % 4] = Math.max(dp[i % 4], stoneValue[i] + stoneValue[i + 1] - dp[(i + 2) % 4]);
+        }
+        if (i + 3 <= n) {
+            dp[i % 4] = Math.max(dp[i % 4], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[(i + 3) % 4]);
+        }
+    }
+    if (dp[0] > 0) {
+        return "Alice";
+    }
+    if (dp[0] < 0) {
+        return "Bob";
+    }
+    return "Tie";
+}
