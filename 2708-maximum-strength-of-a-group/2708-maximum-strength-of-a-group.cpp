70f3283b76f84e842a5a71f873d3d0e852463b7a
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-28 00:28:41 +0530

    Time: 96 ms (100.00%), Space: 46.3 MB (100.00%) - LeetHub

--- 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.cpp
+++ 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.cpp
@@ -1,44 +1,38 @@
-class Solution {
-public:
-    long long maxStrength(vector<int>& nums) {
-        sort(begin(nums), end(nums));
-        long long res = 1, sz = nums.size(), cnt = 0;
-        for (int i = 0; i < sz; ++i)
-            if (res * nums[i] > 0 || (i + 1 < sz && nums[i + 1] < 0)) {
-                res *= nums[i];
-                ++cnt;
-            }
-        return cnt ? res : nums.back();
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxStrength = function(nums) {
+    const size = nums.length;
+
+    if (size === 1) {
+      return nums[0];
     }
-//     long long maxStrength(vector<int> nums) {
-//         int size = nums.size();
 
-//         if (size == 1) {
-//             return nums[0];
-//         }
+    let negativeCount = 0;
+    let maxNegative = Number.MIN_SAFE_INTEGER;
+    let positiveCount = 0;
+    let answer = 1;
+    let zeroCount = 0;
 
-//         long long negativeCount = 0;
-//         int maxNegative = INT_MIN;
-//         long long positiveCount = 0;
-//         long long answer = 1;
-//         int zeroCount = 0;
+    for (const num of nums) {
+      if (num === 0) {
+        zeroCount++;
+        continue;
+      }
+      if (num < 0) {
+        negativeCount++;
+        maxNegative = Math.max(maxNegative, num);
+      } else {
+        positiveCount++;
+      }
+      answer *= Math.abs(num);
+    }
 
-//         for (int num : nums) {
-//             if (num == 0) {
-//                 zeroCount++;
-//                 continue;
-//             }
-//             if (num < 0) {
-//                 negativeCount++;
-//                 maxNegative = max(maxNegative, num);
-//             }
-//             else positiveCount++;
-//             answer *= abs(num);
-//         }
+    if (zeroCount === size) return 0;
+    if (negativeCount + zeroCount === size && negativeCount === 1) return 0;
+    if (negativeCount % 2 === 0) return answer;
+    else return answer / Math.abs(maxNegative);
+  }
 
-//         if (zeroCount == size)return 0;
-//         if (negativeCount + zeroCount == size && negativeCount == 1)return 0;
-//         if (negativeCount % 2 == 0)return answer;
-//         else return answer / abs(maxNegative);
-//     }
-};
+
