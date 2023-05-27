c79d900997a229f6d5fe1d1aabb5fc3e25bc70cb
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-28 00:25:13 +0530

    Time: 9140 ms (5.88%), Space: 29.9 MB (100.00%) - LeetHub

--- 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.cpp
+++ 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.cpp
@@ -1,122 +1,32 @@
-// class Solution {
-// public:
-//     class UnionFind {
-// private:
-//     vector<int> parent;
-//     vector<int> rank;
-
-// public:
-//     UnionFind(int n) {
-//         parent.resize(n);
-//         rank.resize(n, 1);
-//         for (int i = 0; i < n; i++) {
-//             parent[i] = i;
-//         }
-//     }
-
-//     int find(int x) {
-//         if (parent[x] != x) {
-//             parent[x] = find(parent[x]);
-//         }
-//         return parent[x];
-//     }
-
-//     void unite(int x, int y) {
-//         int rootX = find(x);
-//         int rootY = find(y);
-
-//         if (rootX != rootY) {
-//             if (rank[rootX] < rank[rootY]) {
-//                 parent[rootX] = rootY;
-//             } else if (rank[rootX] > rank[rootY]) {
-//                 parent[rootY] = rootX;
-//             } else {
-//                 parent[rootX] = rootY;
-//                 rank[rootY]++;
-//             }
-//         }
-//     }
-// };
-
-// bool canTraverseAllPairs(vector<int>& nums) {
-//     int n = nums.size();
-//     UnionFind uf(n);
-
-//     unordered_map<int, vector<int>> factors;
-
-//     for (int i = 0; i < n; i++) {
-//         for (int j = 2; j * j <= nums[i]; j++) {
-//             if (nums[i] % j == 0) {
-//                 factors[j].push_back(i);
-//                 while (nums[i] % j == 0) {
-//                     nums[i] /= j;
-//                 }
-//             }
-//         }
-//         if (nums[i] > 1) {
-//             factors[nums[i]].push_back(i);
-//         }
-//     }
-
-//     for (auto& factorPair : factors) {
-//         vector<int>& indices = factorPair.second;
-//         for (int i = 1; i < indices.size(); i++) {
-//             uf.unite(indices[i - 1], indices[i]);
-//         }
-//     }
-
-//     int root = uf.find(0);
-//     for (int i = 1; i < n; i++) {
-//         if (uf.find(i) != root) {
-//             return false;
-//         }
-//     }
-
-//     return true;
-// }
- 
-// };
-
-constexpr int maxn = 100000;
-vector<int> pd[maxn + 1];
-class Solution {
-public:
-    bool canTraverseAllPairs(vector<int>& nums) {
-        if (pd[2].empty()) {
-            for (int i = 2; i <= maxn; i += 1) {
-                if (pd[i].empty()) {
-                    for (int j = i; j <= maxn; j += i) {
-                        pd[j].push_back(i);
-                    }
-                }
-            }
-        }
-        map<int, vector<int>> mp;
-        int n = nums.size();
-        vector<vector<int>> g(n);
-        for (int i = 0; i < n; i += 1) {
-            for (int p : pd[nums[i]]) {
-                mp[p].push_back(i);
-            }
-        }
-        for (auto& [x, v] : mp) {
-            for (int i = 1; i < v.size(); i += 1) {
-                g[v[0]].push_back(v[i]);
-                g[v[i]].push_back(v[0]);
-            }
-        }
-        vector<int> vis(n);
-        int m = 0;
-        function<void(int)> dfs = [&](int u) {
-            if (vis[u]) {
-                return;
-            }
-            m += vis[u] = 1;
-            for (int v : g[u]) {
-                dfs(v);
-            }
-        };
-        dfs(0);
-        return n == m;
-    }
-};
+class UnionFind:
+    def __init__(self, n):
+        self.fa = [i for i in range(n)]
+        self.size = [1] * n
+    def getFa(self, i):
+        if i == self.fa[i]:
+            return i
+        self.fa[i] = self.getFa(self.fa[i])
+        return self.fa[i]
+    def merge(self, i, j):
+        fi, fj = self.getFa(i), self.getFa(j)
+        if fi == fj: return fj
+        if self.size[fi] > self.size[fj]: fi, fj = fj, fi
+        self.size[fj] += self.size[fi]
+        self.fa[fi] = fj
+        return fj
+    
+class Solution:
+    def canTraverseAllPairs(self, nums: List[int]) -> bool:
+        fst = [-1] * (1 + max(nums))
+        u = UnionFind(len(nums))
+        for i, x in enumerate(nums):
+            for p in range(2, x):
+                if p * p > x: break
+                if x % p != 0: continue
+                if fst[p] != -1: u.merge(fst[p], i)
+                else: fst[p] = i
+                while x % p == 0: x //= p
+            if x > 1:
+                if fst[x] != -1: u.merge(fst[x], i);
+                else: fst[x] = i
+        return u.size.count(len(nums)) > 0
