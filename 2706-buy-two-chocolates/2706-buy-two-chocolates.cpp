1a830c3ee4197e914a8e7afc3087469a804b005f
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-28 00:11:06 +0530

    Time: 29 ms (40.00%), Space: 46.7 MB (100.00%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,8 +1,25 @@
+// class Solution {
+// public:
+//     int buyChoco(vector<int>& arr, int money) {
+//         sort(begin(arr),end(arr));
+//         return arr[0]+arr[1]<=money?money - (arr[0]+arr[1]) : money;
+        
+//     }
+// };
+
 class Solution {
 public:
-    int buyChoco(vector<int>& arr, int money) {
-        sort(begin(arr),end(arr));
-        return arr[0]+arr[1]<=money?money - (arr[0]+arr[1]) : money;
-        
+    int buyChoco(vector<int>& prices, int money) {
+        int x = INT_MAX,y = INT_MAX;
+        for(auto &i: prices){
+            if(x>i){
+                y = x;
+                x = i;
+            }else if(y>i){
+                y = i;
+            }
+        }
+        if(x+y>money)return money;
+        return money-x-y;
     }
 };
