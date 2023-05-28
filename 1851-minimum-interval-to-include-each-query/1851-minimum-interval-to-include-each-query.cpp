b0293ee75441b49c0fadc5bca08c80c2bbe70e12
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-28 18:38:37 +0530

    Time: 583 ms (71.56%), Space: 121.7 MB (69.73%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
@@ -1,7 +1,13 @@
 class Solution {  // Unordered_Map + Max_Heap
 public:
+    struct comp{
+        bool operator()(pair<int , int>p1, pair<int , int>p2){
+            return p1.first  >=  p2.first;
+        }
+    };
     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
-        priority_queue<vector<int>> pq;
+        // priority_queue<vector<int>> pq;
+        priority_queue<pair<int,int> , vector<pair<int,int>>,comp>pq;
         unordered_map<int, int> result;
         vector<int> sortedQueries = queries, result2;
         sort(intervals.begin(), intervals.end());
@@ -10,11 +16,11 @@ public:
         for (int q : sortedQueries) {
             while (i < n && intervals[i][0] <= q) {
                 int start = intervals[i][0], end = intervals[i++][1];
-                pq.push({start - end - 1, end});
+                pq.push({end - start + 1, end});
             }
-            while (pq.size() && pq.top()[1] < q)
+            while (pq.size() && pq.top().second < q)
                 pq.pop();
-            result[q] = pq.size() ? -pq.top()[0] : -1;
+            result[q] = pq.size() ? pq.top().first : -1;
         }
         for (int q : queries)
             result2.push_back(result[q]);
