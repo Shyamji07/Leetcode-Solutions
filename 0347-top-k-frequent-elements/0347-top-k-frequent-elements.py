8ccf6f6a2ca33d8d400222594e8956ece5ab9e3b
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-28 19:28:27 +0530

    Time: 106 ms (83.57%), Space: 21.2 MB (27.54%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,21 +1,18 @@
-class Solution {
-public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> map;
-        for(int num : nums){
-            map[num]++;
-        }
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hashmap = {}
+        for num in nums:
+            if num in hashmap:
+                hashmap[num] += 1
+            else:
+                hashmap[num] = 1
+        heap = []
+        for key in hashmap:
+            heapq.heappush(heap, (-hashmap[key], key))
         
-        vector<int> res;
-        // pair<first, second>: first is frequency,  second is number
-        priority_queue<pair<int,int>> pq; 
-        for(auto it = map.begin(); it != map.end(); it++){
-            pq.push(make_pair(it->second, it->first));
-            if(pq.size() > (int)map.size() - k){
-                res.push_back(pq.top().second);
-                pq.pop();
-            }
-        }
-        return res;
-    }
-};
+        res = []
+        for _ in range(k):
+            popped = heapq.heappop(heap)
+            res.append(popped[1])
+        
+        return res
