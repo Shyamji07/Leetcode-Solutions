b487a5fa26f63796844214620a786579a945eb22
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-28 18:41:11 +0530

    Time: 623 ms (57.80%), Space: 121.8 MB (65.44%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
@@ -1,39 +1,13 @@
-// class Solution {  // Unordered_Map + Min_Heap
-// public:
-//     struct comp{
-//         bool operator()(pair<int , int>p1, pair<int , int>p2){
-//             return p1.first  >=  p2.first;
-//         }
-//     };
-//     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
-//         // priority_queue<vector<int>> pq;
-//         priority_queue<pair<int,int> , vector<pair<int,int>>,comp>pq;
-//         unordered_map<int, int> result;
-//         vector<int> sortedQueries = queries, result2;
-//         sort(intervals.begin(), intervals.end());
-//         sort(sortedQueries.begin(), sortedQueries.end());
-//         int i = 0, n = intervals.size();
-//         for (int q : sortedQueries) {
-//             while (i < n && intervals[i][0] <= q) {
-//                 int start = intervals[i][0], end = intervals[i++][1];
-//                 pq.push({end - start + 1, end});
-//             }
-//             while (pq.size() && pq.top().second < q)
-//                 pq.pop();
-//             result[q] = pq.size() ? pq.top().first : -1;
-//         }
-//         for (int q : queries)
-//             result2.push_back(result[q]);
-//         return result2;
-//     }
-// };
-
-
-
-class Solution {  // Unordered_Map + Max_Heap
+class Solution {  // Unordered_Map + Min_Heap
 public:
+    struct comp{
+        bool operator()(pair<int , int>p1, pair<int , int>p2){
+            return p1.first  >=  p2.first;
+        }
+    };
     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
-        priority_queue<pair<int,int>> pq;
+        // priority_queue<vector<int>> pq;
+        priority_queue<pair<int,int> , vector<pair<int,int>>,comp>pq;
         unordered_map<int, int> result;
         vector<int> sortedQueries = queries, result2;
         sort(intervals.begin(), intervals.end());
@@ -42,16 +16,42 @@ public:
         for (int q : sortedQueries) {
             while (i < n && intervals[i][0] <= q) {
                 int start = intervals[i][0], end = intervals[i++][1];
-                pq.push({start - end - 1, end});
+                pq.push({end - start + 1, end});
             }
             while (pq.size() && pq.top().second < q)
                 pq.pop();
-            result[q] = pq.size() ? -pq.top().first : -1;
+            result[q] = pq.size() ? pq.top().first : -1;
         }
         for (int q : queries)
             result2.push_back(result[q]);
         return result2;
     }
 };
+
+
+
+// class Solution {  // Unordered_Map + Max_Heap
+// public:
+//     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
+//         priority_queue<pair<int,int>> pq;
+//         unordered_map<int, int> result;
+//         vector<int> sortedQueries = queries, result2;
+//         sort(intervals.begin(), intervals.end());
+//         sort(sortedQueries.begin(), sortedQueries.end());
+//         int i = 0, n = intervals.size();
+//         for (int q : sortedQueries) {
+//             while (i < n && intervals[i][0] <= q) {
+//                 int start = intervals[i][0], end = intervals[i++][1];
+//                 pq.push({start - end - 1, end});
+//             }
+//             while (pq.size() && pq.top().second < q)
+//                 pq.pop();
+//             result[q] = pq.size() ? -pq.top().first : -1;
+//         }
+//         for (int q : queries)
+//             result2.push_back(result[q]);
+//         return result2;
+//     }
+// };
 
 
