b64a08c06a1ebe0e5362922c615c0f0752972d99
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-28 11:30:07 +0530

    Time: 91 ms (100.00%), Space: 45.7 MB (100.00%) - LeetHub

--- 2712-minimum-cost-to-make-all-characters-equal/2712-minimum-cost-to-make-all-characters-equal.cpp
+++ 2712-minimum-cost-to-make-all-characters-equal/2712-minimum-cost-to-make-all-characters-equal.cpp
@@ -1,12 +1,12 @@
-class Solution {
-public:
-    long long minimumCost(string s) {
-        long long ans=0;
-        for(int i = 0;i+1<s.size();i++){
-            if(s[i]!=s[i+1]){
-                ans+=min(i+1,(int)s.size()-i-1);
-            }
-        }
-        return ans;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minimumCost = function(s) {
+    let a = 0;
+    for (let i = 1; i < s.length; i++) {
+        a += s[i - 1] === s[i] ? 0 : Math.min(i, s.length - i);
     }
+    return a;
+
 };
