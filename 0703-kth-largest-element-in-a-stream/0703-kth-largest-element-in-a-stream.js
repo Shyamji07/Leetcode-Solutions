187d5ad29cdad3fd4788ca353b3a4f29b79c2c8c
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-28 19:33:27 +0530

    Time: 125 ms (83.07%), Space: 50 MB (76.17%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.js
@@ -1,19 +1,117 @@
-import heapq
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function(k, nums) {
+  this.k = k;
+  this.heap = new MinHeap();
+  nums.forEach(n => this.add(n));
+};
 
-class KthLargest:
-    def __init__(self, k: int, nums: List[int]):
-        self.num_elems = k
-        self.pq_elems = []
-        for num in nums:
-            heapq.heappush(self.pq_elems, num)
-            if len(self.pq_elems) > k:
-                heapq.heappop(self.pq_elems)
+/**
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+  if (this.heap.size() < this.k) {
+    this.heap.offer(val);
+  } else if (this.heap.peek() < val) {
+    this.heap.offer(val);
+    this.heap.poll();
+  }
+  return this.heap.peek();
+};
 
-    def add(self, val: int) -> int:
-        if len(self.pq_elems) < self.num_elems:
-            heapq.heappush(self.pq_elems, val)
-        else:
-            if val > self.pq_elems[0]:
-                heapq.heappop(self.pq_elems)
-                heapq.heappush(self.pq_elems, val)
-        return self.pq_elems[0]
+class MinHeap {
+  constructor(data = []) {
+    this.data = data;
+    this.comparator = (a, b) => a - b;
+    this.heapify();
+  }
+
+  // O(nlog(n))
+  heapify() {
+    if (this.size() < 2) return;
+    for (let i = 1; i < this.size(); i++) {
+      this.bubbleUp(i);
+    }
+  }
+
+  // O(1)
+  peek() {
+    if (this.size() === 0) return null;
+    return this.data[0];
+  }
+
+  // O(log(n))
+  offer(value) {
+    this.data.push(value);
+    this.bubbleUp(this.size() - 1);
+  }
+
+  // O(log(n))
+  poll() {
+    if (this.size() === 0) return null;
+    const result = this.data[0];
+    const last = this.data.pop();
+    if (this.size() !== 0) {
+      this.data[0] = last;
+      this.bubbleDown(0);
+    }
+    return result;
+  }
+
+  // O(log(n))
+  bubbleUp(index) {
+    while (index > 0) {
+      const parentIndex = (index - 1) >> 1;
+      if (this.comparator(this.data[index], this.data[parentIndex]) < 0) {
+        this.swap(index, parentIndex);
+        index = parentIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  // O(log(n))
+  bubbleDown(index) {
+    const lastIndex = this.size() - 1;
+    while (true) {
+      const leftIndex = index * 2 + 1;
+      const rightIndex = index * 2 + 2;
+      let findIndex = index;
+      if (
+        leftIndex <= lastIndex &&
+        this.comparator(this.data[leftIndex], this.data[findIndex]) < 0
+      ) {
+        findIndex = leftIndex;
+      }
+      if (
+        rightIndex <= lastIndex &&
+        this.comparator(this.data[rightIndex], this.data[findIndex]) < 0
+      ) {
+        findIndex = rightIndex;
+      }
+      if (index !== findIndex) {
+        this.swap(index, findIndex);
+        index = findIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  // O(1)
+  swap(index1, index2) {
+    [this.data[index1], this.data[index2]] = [
+      this.data[index2],
+      this.data[index1]
+    ];
+  }
+
+  // O(1)
+  size() {
+    return this.data.length;
+  }
+}
