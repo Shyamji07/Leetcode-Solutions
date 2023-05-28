2886db03ccde24e0343075268d98c48d01874949
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-28 18:39:17 +0530

    Time: 597 ms (65.75%), Space: 121.7 MB (73.09%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
@@ -1,4 +1,4 @@
-class Solution {  // Unordered_Map + Max_Heap
+class Solution {  // Unordered_Map + Min_Heap
 public:
     struct comp{
         bool operator()(pair<int , int>p1, pair<int , int>p2){
@@ -27,3 +27,32 @@ public:
         return result2;
     }
 };
+
+
+/*
+
+class Solution {  // Unordered_Map + Max_Heap
+public:
+    vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
+        priority_queue<vector<int>> pq;
+        unordered_map<int, int> result;
+        vector<int> sortedQueries = queries, result2;
+        sort(intervals.begin(), intervals.end());
+        sort(sortedQueries.begin(), sortedQueries.end());
+        int i = 0, n = intervals.size();
+        for (int q : sortedQueries) {
+            while (i < n && intervals[i][0] <= q) {
+                int start = intervals[i][0], end = intervals[i++][1];
+                pq.push({start - end - 1, end});
+            }
+            while (pq.size() && pq.top()[1] < q)
+                pq.pop();
+            result[q] = pq.size() ? -pq.top()[0] : -1;
+        }
+        for (int q : queries)
+            result2.push_back(result[q]);
+        return result2;
+    }
+};
+
+*/
