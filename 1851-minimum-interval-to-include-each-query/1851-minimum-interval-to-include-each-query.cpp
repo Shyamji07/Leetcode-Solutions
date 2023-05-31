ebaeeb1c91e5fa8100f3acda455e4a888a3226e7
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-05-31 07:48:30 +0530

    Time: 577 ms (73.64%), Space: 121.7 MB (73.03%) - LeetHub

--- 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
@@ -1,56 +1,30 @@
-class Solution {  // Unordered_Map + Min_Heap
+#define pi pair<int , int> 
+class Solution {
 public:
     struct comp{
-        bool operator()(pair<int , int>p1, pair<int , int>p2){
-            return p1.first  >=  p2.first;
+        bool operator()(pi p1 , pi p2){
+            return p1.first >= p2.first;
         }
     };
     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
-        priority_queue<pair<int,int> , vector<pair<int,int>>,comp>pq;
-        unordered_map<int, int> result;
-        vector<int> sortedQueries = queries, result2;
-        sort(intervals.begin(), intervals.end());
-        sort(sortedQueries.begin(), sortedQueries.end());
+        priority_queue<pi , vector<pi> , comp>pq;   //{interval length , end_time} sort length come first
+        unordered_map<int , int>mp;                 // for storing answer as because original answer is not in sorted order 
+        vector<int>Q = queries, res;
+        sort(begin(intervals) , end(intervals));
+        sort(begin(Q) , end(Q));
+        
         int i = 0, n = intervals.size();
-        for (int q : sortedQueries) {
-            while (i < n && intervals[i][0] <= q) {
-                int start = intervals[i][0], end = intervals[i++][1];
-                pq.push({end - start + 1, end});
+        for(int q : Q){
+            while(i < n and intervals[i][0] <= q){ // starting point is lesser than q then add into the queue
+                int start = intervals[i][0] , end = intervals[i++][1];
+                pq.push({end - start + 1 , end});
             }
-            while (!pq.empty() && pq.top().second < q)
+            while(!pq.empty() and pq.top().second < q) //if end point is also less than q then it mean q is out of the range of interval 
                 pq.pop();
-            result[q] = !pq.empty() ? pq.top().first : -1;
+            mp[q] = !pq.empty() ? pq.top().first : -1;    // else map querry to length
         }
-        for (int q : queries)
-            result2.push_back(result[q]);
-        return result2;
+        for(int q : queries)      // push answer in desired order
+            res.push_back(mp[q]);
+        return res;
     }
 };
-
-
-
-// class Solution {  // Unordered_Map + Max_Heap
-// public:
-//     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
-//         priority_queue<pair<int,int>> pq;
-//         unordered_map<int, int> result;
-//         vector<int> sortedQueries = queries, result2;
-//         sort(intervals.begin(), intervals.end());
-//         sort(sortedQueries.begin(), sortedQueries.end());
-//         int i = 0, n = intervals.size();
-//         for (int q : sortedQueries) {
-//             while (i < n && intervals[i][0] <= q) {
-//                 int start = intervals[i][0], end = intervals[i++][1];
-//                 pq.push({start - end - 1, end});
-//             }
-//             while (pq.size() && pq.top().second < q)
-//                 pq.pop();
-//             result[q] = pq.size() ? -pq.top().first : -1;
-//         }
-//         for (int q : queries)
-//             result2.push_back(result[q]);
-//         return result2;
-//     }
-// };
-
-
