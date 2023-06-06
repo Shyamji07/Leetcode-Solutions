531ec9d70097954a15e472ffbd172094010b21b0
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-06-06 07:09:30 +0530

    Time: 66 ms (6.09%), Space: 16.4 MB (20.83%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -0,0 +1,7 @@
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        m = min(arr)
+        gap = (max(arr) - m) / (len(arr) - 1)
+        if gap == 0: return True
+        s = set(num - m for num in arr)
+        return len(s) == len(arr) and all(diff % gap == 0 for diff in s)
