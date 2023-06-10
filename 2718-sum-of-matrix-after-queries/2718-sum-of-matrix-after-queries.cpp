6ba30a1c64cbe165779566479fd7a422d3f6c758
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-06-10 20:17:48 +0530

    Time: 580 ms (88.37%), Space: 190.4 MB (96.27%) - LeetHub

--- 2718-sum-of-matrix-after-queries/2718-sum-of-matrix-after-queries.cpp
+++ 2718-sum-of-matrix-after-queries/2718-sum-of-matrix-after-queries.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     long long matrixSumQueries(int n, vector<vector<int>>& qs) {
-        long long res = 0, cols_cnt = n, rows_cnt = n;
-        vector<bool> cols(n), rows(n);
+        long long res = 0, cnt[2] = {n, n};
+        bool seen[2][10001] = {};
         for (int i = qs.size() - 1; i >= 0; --i) {
             int type = qs[i][0], id = qs[i][1], v = qs[i][2];
-            if (type == 0) { // row
-                if (!rows[id]) {
-                    rows[id] = true;
-                    --rows_cnt;
-                    res += v * cols_cnt;
-                }
-            }
-            else { // col
-                if (!cols[id]) {
-                    cols[id] = true;
-                    --cols_cnt;    
-                    res += v * rows_cnt;
-                }
+            if (!seen[type][id]) {
+                seen[type][id] = true;
+                res += v * cnt[!type];
+                --cnt[type];
             }
         }
         return res;
