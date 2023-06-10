ad1a6afa0fac41d43b223441f949cf2c4650e12e
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-06-10 20:11:18 +0530

    Time: 91 ms (47.10%), Space: 16.1 MB (97.60%) - LeetHub

--- 2717-semi-ordered-permutation/2717-semi-ordered-permutation.py
+++ 2717-semi-ordered-permutation/2717-semi-ordered-permutation.py
@@ -1,9 +1,6 @@
-class Solution {
-public:
-    int semiOrderedPermutation(vector<int>& A) {
-        int n = A.size();
-        int i = find(A.begin(), A.end(), 1) - A.begin();
-        int j = find(A.begin(), A.end(), n) - A.begin();
-        return i + n - 1 - j - (i > j);
-    }
-};
+class Solution:
+    def semiOrderedPermutation(self, A: List[int]) -> int:
+        n = len(A)
+        i, j = A.index(1), A.index(n)
+        return i + n - 1 - j - (i > j)
+        
