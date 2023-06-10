220fd339151609eacbebe57f242b58bd6b018e33
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-06-10 19:41:22 +0530

    Time: 2 ms (50.23%), Space: 5.9 MB (46.01%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    bool check(int n, int i, int maxSum, int m) {
-        for (int d = 1; maxSum >= 0 && d < m && i + d < n; ++d)
-            maxSum -= m - d;
-        for (int d = 1; maxSum >= 0 && d < m && i - d >= 0; ++d)
-            maxSum -= m - d;
-        return maxSum >= 0;
-    }
     int maxValue(int n, int index, int maxSum) {
-        int l = 0, r = maxSum;
-        while (l < r) {
-            int m = (l + r + 1) / 2;
-            if (check(n, index, maxSum - n - m, m))
-                l = m;
+        maxSum -= n;
+        int left = 0, right = maxSum, mid;
+        while (left < right) {
+            mid = (left + right + 1) / 2;
+            if (test(n, index, mid) <= maxSum)
+                left = mid;
             else
-                r = m - 1;
+                right = mid - 1;
         }
-        return l + 1;
+        return left + 1;
+    }
+
+    long test(int n, int index, int a) {
+        int b = max(a - index, 0);
+        long res = long(a + b) * (a - b + 1) / 2;
+        b = max(a - ((n - 1) - index), 0);
+        res += long(a + b) * (a - b + 1) / 2;
+        return res - a;
     }
 };
