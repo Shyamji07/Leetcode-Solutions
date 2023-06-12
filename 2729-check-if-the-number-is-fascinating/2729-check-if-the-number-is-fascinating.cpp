8b711ee786ba2779829663e45b8f1e6a2bd96970
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-06-12 07:31:21 +0530

    Time: 3 ms (72.73%), Space: 6 MB (54.55%) - LeetHub

--- 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.cpp
+++ 2729-check-if-the-number-is-fascinating/2729-check-if-the-number-is-fascinating.cpp
@@ -1,8 +1,28 @@
 class Solution {
 public:
     bool isFascinating(int n) {
-        string str= to_string(n)+to_string(2*n)+to_string(3*n);
-        sort(str.begin(),str.end());
-        return str == "123456789";
+        string str = "";
+        int a = n*2,b=n*3;
+        str = to_string(n)+to_string(a)+to_string(b);
+        vector<int> v(10,0);
+        for(auto &i: str){
+            v[i-'0']++;
+        }
+        for(int i = 0; i < 10; i++){
+            if(i==0){
+                if(v[i])return false;
+            }else{
+                if(v[i]!=1)return false;
+            }
+        }
+        return true;
     }
 };
+// class Solution {
+// public:
+//     bool isFascinating(int n) {
+//         string str= to_string(n)+to_string(2*n)+to_string(3*n);
+//         sort(str.begin(),str.end());
+//         return str == "123456789";
+//     }
+// };
