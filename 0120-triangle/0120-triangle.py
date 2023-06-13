2c4862211c8f97d651ba0553591a1ac1663aeb97
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-06-13 07:54:57 +0530

    Time: 74 ms (76.89%), Space: 17.4 MB (37.00%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,6 +1,6 @@
-var minimumTotal = function(T) {
-    for (let i = T.length - 2; ~i; i--) 
-        for (let j = T[i].length - 1; ~j; j--) 
-            T[i][j] += Math.min(T[i+1][j], T[i+1][j+1])
-    return T[0][0]
-}
+class Solution:
+    def minimumTotal(self, T: List[List[int]]) -> int:
+        for i in range(len(T)-2,-1,-1):
+            for j in range(len(T[i])-1,-1,-1):
+                T[i][j] += min(T[i+1][j], T[i+1][j+1])
+        return T[0][0]
