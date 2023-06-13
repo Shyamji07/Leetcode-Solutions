747bec06fb814f440b23ace466c6b6cc10bc7139
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-06-13 07:56:50 +0530

    Time: 11 ms (22.30%), Space: 8.5 MB (98.34%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,10 +1,28 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> T) {
-        for (int i = T.size() - 2; i >= 0; i--) 
-            for (int j = T.get(i).size() - 1; j >= 0; j--) {
-                int min = Math.min(T.get(i+1).get(j), T.get(i+1).get(j+1));
-                T.get(i).set(j, T.get(i).get(j) + min);
+public:
+    int minimumTotal(vector<vector<int>>& t) {
+        //bottom up dp
+        int n = t.size();
+        for(int i=n-2;i>=0;i--)
+        {
+            for(int j=0;j<t[i].size();j++)
+            {
+                t[i][j] += min(t[i+1][j],t[i+1][j+1]);
             }
-        return T.get(0).get(0);
+        }
+        return t[0][0];
     }
-}
+};
+// class Solution {
+// public:
+//     int minimumTotal(vector<vector<int>>& v) {
+//         for(int i=1;i<v.size();i++){
+//             v[i][0]+=v[i-1][0];
+//             v[i][v[i].size()-1]+=v[i-1][v[i-1].size()-1];
+//             for(int j=1;j<v[i].size()-1;j++){
+//                 v[i][j]+=min(v[i-1][j-1],v[i-1][j]);
+//             }
+//         }
+//         return *min_element(v[v.size()-1].begin(),v[v.size()-1].end());
+//     }
+// };
