3267fe7c4e6dfe5b53d7a26cfe8c0e46347aaac5
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-06-14 08:34:17 +0530

    Time: 242 ms (42.05%), Space: 11.4 MB (50.24%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,6 +1,59 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
+      
+        int n=s.size();
+        int dp[n][n];
+        memset(dp,0,sizeof(dp));
+        
+        int maxLen=1;
+        
+        //for substr of length 1
+        for(int i=0;i<n;i++)
+        {
+            dp[i][i]=1;
+        }
+        
+        //for substr of length 2
+        int start=0;
+        for(int i=0;i<n-1;i++)
+        {
+           if(s[i]==s[i+1]) 
+           {
+               dp[i][i+1]=1;
+               start=i;
+               maxLen=2;
+           }
+        }
+        
+        //sustr of length _k_
+        for(int k=3;k<=n;k++)
+        {
+            for(int i=0;i<n-k+1;i++)
+            {
+                int j=i+k-1;
+                if(dp[i+1][j-1]==1 && s[i]==s[j])
+                {
+                    dp[i][j]=1;
+                    if(k>maxLen)
+                    {
+                        start=i;
+                        maxLen=k;
+                    }
+                }
+            }
+        }
+        
+        return s.substr(start,maxLen);
+    }
+};
+
+/*
+
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
         int n=s.size();
         if(n<=1)return s;
         int ans=1;
@@ -66,3 +119,4 @@ public:
 //         return s.substr(minL, maxLen);
 //     }
 // };
+*/
