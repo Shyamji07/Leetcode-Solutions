3bf5aa6b3dab694957ba256bd2196299de5eda50
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-06-28 07:45:39 +0530

    Time: 118 ms (60.04%), Space: 19.9 MB (87.36%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,79 +1,31 @@
-// Heap Approach
-class Solution {
-public:
-
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
-        if(lists.empty()) return NULL;
-        
-        // MAX heap
-        priority_queue<pair<int, ListNode*>> pq; 
-        int n=lists.size();
-        for(int i=0;i<n;i++)
-        {
-            ListNode *p = lists[i];
-            while(p)
-            {
-                pq.push({-p->val, p}); // acts as min heap
-                p = p->next;
-            }
-        }
-                
-        ListNode* head=NULL;
-        ListNode* last=NULL;
-        
-        if(pq.empty()) return head;
-        
-        head=pq.top().second;
-        last=pq.top().second;
-        pq.pop();
-        
-        
-        while(!pq.empty())
-        {
-            last->next = pq.top().second;
-            last = pq.top().second;
-            pq.pop();
-            
-        }
-        last->next=NULL;
-        
-        return head;
-        
-    }
-};
-
-
-//DNC Approach
-
-
-
-// class Solution {
-// public:
-//     ListNode* merge(ListNode* l1, ListNode* l2){
-//         if(!l1)return l2;
-//         if(!l2)return l1;
-//             if(l1->val<=l2->val){
-//                  l1->next=merge(l1->next,l2);
-//                 return l1;
-//             }
-//             else {
-//                 l2->next=merge(l1,l2->next);
-//                 return l2;
-//         }
-//         return l1;
-//     }
-//     ListNode* divide(vector<ListNode*>& lists,int l,int r){
-//         if(l>r)return NULL;
-//         if(l==r)return lists[l];
-//         int mid=l+(r-l)/2;
-//         ListNode *left=divide(lists,l,mid);
-//         ListNode * right=divide(lists,mid+1,r);
-//         lists[l]=merge(left, right);
-//         return lists[l];
-//     }
-//     ListNode* mergeKLists(vector<ListNode*>& lists) {
-//         if(lists.size()==0)return nullptr;
-//         return divide(lists,0,lists.size()-1);
-//     }
-// };
+class Solution(object):
+    def mergeKLists(self, lists):
+        if not lists:
+            return None
+        if len(lists) == 1:
+            return lists[0]
+        mid = len(lists) // 2
+        l, r = self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:])
+        return self.merge(l, r)
+    
+    def merge(self, l, r):
+        dummy = p = ListNode()
+        while l and r:
+            if l.val < r.val:
+                p.next = l
+                l = l.next
+            else:
+                p.next = r
+                r = r.next
+            p = p.next
+        p.next = l or r
+        return dummy.next
+    
+    def merge1(self, l, r):
+        if not l or not r:
+            return l or r
+        if l.val< r.val:
+            l.next = self.merge(l.next, r)
+            return l
+        r.next = self.merge(l, r.next)
+        return r
