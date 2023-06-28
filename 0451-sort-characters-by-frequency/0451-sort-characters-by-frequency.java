e7f9de9cd594d1105381ed33fd3d852207a2a1a8
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-06-28 07:53:45 +0530

    Time: 18 ms (58.64%), Space: 44.5 MB (49.45%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -21,3 +21,36 @@ class Solution {
 	return sb.toString();
 }
 }
+
+
+
+// class Solution {
+// public:
+//     string frequencySort(string s) {
+//         unordered_map<char,int> freq;           //for frequency of characters
+//         priority_queue<pair<int,char>> maxheap; //maxheap according to frequency of characters
+//         for(char c: s)
+//             freq[c]++;
+//         for(auto it: freq)
+//             maxheap.push({it.second,it.first}); //heap will be constructed on the basis of frequency
+//         s="";   
+//         while(!maxheap.empty()){
+//             s+=string(maxheap.top().first,maxheap.top().second); //frequency times the character
+//             maxheap.pop();
+//         }
+//         return s;
+//     }
+    
+    
+    
+    
+//     // string frequencySort(string s) {
+//     //     int counts[256] = {0};
+//     //     for (char ch : s)
+//     //         ++counts[ch];
+//     //     sort(s.begin(), s.end(), [&](char a, char b) { 
+//     //         return counts[a] > counts[b] || (counts[a] == counts[b] && a < b); 
+//     //     });
+//     //     return s;
+//     // }
+// };
