a4a5dc940a9e79f0ffc8ee17f077a30488e0f318
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-07-04 00:25:00 +0530

    Time: 155 ms (19.91%), Space: 47.1 MB (60.44%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,10 +1,43 @@
-class Solution {
-public: // T.C -->O(N)+O(KlogN)===>O(NlogN) S.C ==>O(N)
+class Solution {  //T.C ==> O(n.logK)
+public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> pq(nums.begin(),nums.end()); //T.C ->O(N)
-        while(--k>0){   //T.C--> O(KlogN);
-            pq.pop();
+        priority_queue<int> heap;
+        for (int num: nums) {
+            heap.push(-num);
+            if (heap.size() > k) {
+                heap.pop();
+            }
         }
-        return pq.top();
+        
+        return -heap.top();
     }
 };
+
+// class Solution {
+// public: // T.C -->O(N)+O(KlogN)===>O(NlogN) S.C ==>O(N)
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int> pq(nums.begin(),nums.end()); //T.C ->O(N)
+//         while(--k>0){   //T.C--> O(KlogN);
+//             pq.pop();
+//         }
+//         return pq.top();
+//     }
+// };
+
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         partial_sort(nums.begin(), nums.begin() + k, nums.end(), greater<int>());
+//         return nums[k - 1];
+//     }
+// };
+
+// //MERGESORT ALGORITHM
+
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         nth_element(nums.begin(),nums.begin()+k-1,nums.end(),std::greater<int>());
+//         return nums[k-1];
+//     }
+// };
