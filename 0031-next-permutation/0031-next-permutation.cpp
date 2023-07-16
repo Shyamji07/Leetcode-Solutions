07c22a510d09be24f5163f17f7d08c1415c94c3b
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-07-16 22:54:16 +0530

    Time: 4 ms (72.04%), Space: 12.1 MB (19.75%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& n) {
-     next_permutation(n.begin(),n.end());
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size(),k,i;
+        for( k=n-2;k>=0;k--){
+            if(nums[k+1]>nums[k])break;
+        }
+        if(k<0){
+            reverse(nums.begin(),nums.end());
+        }
+        else{
+            for( i=n-1;i>k;i--){
+                if(nums[i]>nums[k]){
+              swap(nums[k],nums[i]);
+                    break;
+            } 
+            }
+            sort(nums.begin()+k+1,nums.end());
+        }
     }
 };
+
+// class Solution {
+// public:
+//     void nextPermutation(vector<int>& n) {
+//      next_permutation(n.begin(),n.end());
+//     }
+// };
