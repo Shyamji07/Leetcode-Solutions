c173f41c4e0f42feaae42e3760a1230a65491313
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-07-16 23:03:55 +0530

    Time: 86 ms (29.67%), Space: 45.7 MB (40.12%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.js
@@ -0,0 +1,16 @@
+function combinationSum(candidates, target) {
+  var buffer = [];
+  var result = [];
+  search(0, target);
+  return result;
+
+  function search(startIdx, target) {
+    if (target === 0) return result.push(buffer.slice());
+    if (target < 0) return;
+    if (startIdx === candidates.length) return;
+    buffer.push(candidates[startIdx]);
+    search(startIdx, target - candidates[startIdx]);
+    buffer.pop();
+    search(startIdx + 1, target);
+  }
+}
