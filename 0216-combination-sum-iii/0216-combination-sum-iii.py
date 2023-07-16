59d2635e26304d4992843e7994fb9c482d1fe1f3
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-07-16 23:06:03 +0530

    Time: 39 ms (94.76%), Space: 16.3 MB (84.39%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,24 +1,17 @@
-class Solution {
-public:
-    vector<vector<int>>ans;
-     vector<int>curr;
-    void comb(vector<int>&curr,int k,int n,int index){
-         if(n<0)return;
-        if(k==0 and n==0){
-            ans.push_back(curr);
-            return;
-        }
-        for(int i=index;i<=9;i++){
-            curr.push_back(i);
-            comb(curr,k-1,n-i,i+1);//last parameter i+1 not index+1
-            curr.pop_back();
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        self.result =[]
+        self.helper([],1,k,n)
+        return self.result
+    
+    def helper(self,path,start,k,target):
         
-        }
-    }
-    vector<vector<int>> combinationSum3(int k, int n) {
-       
-        if(k*(k+1)/2>n)return {};
-        comb(curr,k,n,1);
-        return ans;
-    }
-};
+        if k ==0 and target==0:
+            self.result.append(path)
+            return 
+        
+        if k == 0 or target <= 0:
+            return
+        
+        for i in range(start,10):
+            self.helper(path+[i],i+1,k-1,target-i)
