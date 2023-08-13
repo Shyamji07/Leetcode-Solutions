ea70da252a4522c6af2c89fd3c6f9fb8cfe19348
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-08-13 20:07:00 +0530

    Time: 1114 ms (5.05%), Space: 68.7 MB (5.94%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -10,24 +10,30 @@
 #         curr.pop()
 
 
-class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:
-        def backtrack(curr, first_num):
-            if len(curr) == k:
-                ans.append(curr[:])
-                return
+# class Solution:
+#     def combine(self, n: int, k: int) -> List[List[int]]:
+#         def backtrack(curr, first_num):
+#             if len(curr) == k:
+#                 ans.append(curr[:])
+#                 return
 
-            need = k - len(curr)
-            remain = n - first_num + 1
-            available = remain - need
+#             need = k - len(curr)
+#             remain = n - first_num + 1
+#             available = remain - need
             
-            for num in range(first_num, first_num + available + 1):
-                curr.append(num)
-                backtrack(curr, num + 1)
-                curr.pop()
+#             for num in range(first_num, first_num + available + 1):
+#                 curr.append(num)
+#                 backtrack(curr, num + 1)
+#                 curr.pop()
 
-            return
+#             return
         
-        ans = []
-        backtrack([], 1)
-        return ans
+#         ans = []
+#         backtrack([], 1)
+#         return ans
+
+class Solution:
+    def combine(self, n, k):
+        if k == 0:
+            return [[]]
+        return [pre + [i] for i in range(k, n+1) for pre in self.combine(i-1, k-1)]
