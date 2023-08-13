e924a710f448474a8ed042795b80d5cea2391529
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-08-13 19:02:37 +0530

    Time: 902 ms (90.00%), Space: 30.3 MB (78.46%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,22 +1,43 @@
+# class Solution:
+#     def validPartition(self, nums: List[int]) -> bool:
+#         n = len(nums)
+#         memo = {-1: True}
+
+#         # Determine if the prefix array nums[0 ~ i] has a valid partition
+#         def prefixIsValid(i):
+#             if i in memo:
+#                 return memo[i]
+#             ans = False
+
+#             # Check 3 possibilities
+#             if i > 0 and nums[i] == nums[i - 1]:
+#                 ans |= prefixIsValid(i - 2)
+#             if i > 1 and nums[i] == nums[i - 1] == nums[i - 2]:
+#                 ans |= prefixIsValid(i - 3)
+#             if i > 1 and nums[i] == nums[i - 1] + 1 == nums[i - 2] + 2:
+#                 ans |= prefixIsValid(i - 3)
+#             memo[i] = ans
+#             return ans
+
+#         return prefixIsValid(n - 1)
+
+
+
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
         n = len(nums)
-        memo = {-1: True}
+        dp = [True] + [False] * n
 
         # Determine if the prefix array nums[0 ~ i] has a valid partition
-        def prefixIsValid(i):
-            if i in memo:
-                return memo[i]
-            ans = False
+        for i in range(n):
+            dp_index = i + 1
 
             # Check 3 possibilities
             if i > 0 and nums[i] == nums[i - 1]:
-                ans |= prefixIsValid(i - 2)
+                dp[dp_index] |= dp[dp_index - 2]
             if i > 1 and nums[i] == nums[i - 1] == nums[i - 2]:
-                ans |= prefixIsValid(i - 3)
+                dp[dp_index] |= dp[dp_index - 3]
             if i > 1 and nums[i] == nums[i - 1] + 1 == nums[i - 2] + 2:
-                ans |= prefixIsValid(i - 3)
-            memo[i] = ans
-            return ans
-
-        return prefixIsValid(n - 1)
+                dp[dp_index] |= dp[dp_index - 3]
+ 
+        return dp[n]
