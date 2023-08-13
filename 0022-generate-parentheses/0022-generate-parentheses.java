b2d09f49a3a16c52f5788ec4d8dc85dbdf50e68b
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-08-13 19:34:28 +0530

    Time: 1 ms (94.02%), Space: 42 MB (88.56%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,18 +1,23 @@
-var generateParenthesis = function(N) {
-    let ans = [], m = 2 * N
-    
-    const dfs = (pos, open, seq) => {
-        if (pos === m) {
-            let res = new Array(m)
-            for (let i = 0; i < m; i++)
-                res[i] = seq & 1 << i ? "(" : ")"
-            ans.push(res.join(""))
-            return
-        }
-        if (open) dfs(pos+1, open-1, seq)
-        if (m - pos > open) dfs(pos+1, open+1, seq | 1 << pos)
+class Solution {
+    public List<String> generateParenthesis(int N) {
+        ans = new ArrayList<>();
+        m = 2 * N;
+        dfs(0, 0, 0);
+        return ans;
     }
     
-    dfs(0, 0, 0)
-    return ans
-};
+    private List<String> ans;
+    private int m;
+    
+    private void dfs(int pos, int open, int seq) {
+        if (pos == m) {
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < m; i++)
+                res.append((seq & 1 << i) > 0 ? "(" : ")");
+            ans.add(res.toString());
+            return;
+        }
+        if (open > 0) dfs(pos+1, open-1, seq);
+        if (m - pos > open) dfs(pos+1, open+1, seq | 1 << pos);
+    }
+}
