2a935a02c99693f10bde7e101a5c33a5736c7c4e
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-08-13 19:31:24 +0530

    Time: 4 ms (61.55%), Space: 14.8 MB (37.49%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,39 +1,39 @@
-// class Solution {
-// public:
-//     void dfs(vector<string>&res,string s, int l,int r,int n){
-//         if(l==n and r==n){
-//             res.push_back(s);
-//             return;
-//         }
-//         if(l<n)dfs(res,s+'(',l+1,r,n);
-//         if(r<l)dfs(res,s+')',l,r+1,n);
-//     }
-//     vector<string> generateParenthesis(int n) {
-//         vector<string>res;
-//         dfs(res,"",0,0,n);
-//         return res;
-//     }
-// };
 class Solution {
 public:
-    vector<string> generateParenthesis(int n);
-};
-// dp as binary tree generate
-vector<string> Solution::generateParenthesis(int n) {
-    vector<vector<string>> dp(n + 1, vector<string>());
-    dp[0] = {""};
-    for (int i = 1; i <= n; ++i) { // total
-        for (int left = 0; left <= i - 1; ++left) { // left tree size
-            int right = i - 1 - left;
-            for (int l = 0; l < dp[left].size(); ++l) {
-                for (int r = 0; r < dp[right].size(); ++r) {
-                    dp[i].push_back("(" + dp[left][l] + ")" + dp[right][r]);
-                }
-            }
+    void dfs(vector<string>&res,string s, int l,int r,int n){
+        if(l==n and r==n){
+            res.push_back(s);
+            return;
         }
+        if(l<n)dfs(res,s+'(',l+1,r,n);
+        if(r<l)dfs(res,s+')',l,r+1,n);
     }
-    return dp[n];
-}
+    vector<string> generateParenthesis(int n) {
+        vector<string>res;
+        dfs(res,"",0,0,n);
+        return res;
+    }
+};
+// class Solution {
+// public:
+//     vector<string> generateParenthesis(int n);
+// };
+// // dp as binary tree generate
+// vector<string> Solution::generateParenthesis(int n) {
+//     vector<vector<string>> dp(n + 1, vector<string>());
+//     dp[0] = {""};
+//     for (int i = 1; i <= n; ++i) { // total
+//         for (int left = 0; left <= i - 1; ++left) { // left tree size
+//             int right = i - 1 - left;
+//             for (int l = 0; l < dp[left].size(); ++l) {
+//                 for (int r = 0; r < dp[right].size(); ++r) {
+//                     dp[i].push_back("(" + dp[left][l] + ")" + dp[right][r]);
+//                 }
+//             }
+//         }
+//     }
+//     return dp[n];
+// }
 // Time Complexity: O(2^n). 
 // For every index there can be two options ‘{‘ or ‘}’. So it can be said that the upper bound of time complexity is O(2^n) or it has exponential time complexity.
 
