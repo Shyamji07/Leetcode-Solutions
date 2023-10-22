30ac6db811d2333eacd58330dc4fd8126adff184
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2023-10-22 17:09:51 +0900

    Time: 1235 ms (5.10%), Space: 27.5 MB (10.20%) - LeetHub

---
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maximumScore(self, nums: List[int], k: int) -> int:
+        def solve(nums, k):
+            n = len(nums)
+            left = [0] * k
+            curr_min = inf
+            for i in range(k - 1, -1, -1):
+                curr_min = min(curr_min, nums[i])
+                left[i] = curr_min
+
+            right = []
+            curr_min = inf
+            for i in range(k, n):
+                curr_min = min(curr_min, nums[i])
+                right.append(curr_min)
+
+            ans = 0
+            for j in range(len(right)):
+                curr_min = right[j]
+                i = bisect_left(left, curr_min)
+                size = (k + j) - i + 1
+                ans = max(ans, curr_min * size)
+                
+            return ans
+        
+        return max(solve(nums, k), solve(nums[::-1], len(nums) - k - 1))
