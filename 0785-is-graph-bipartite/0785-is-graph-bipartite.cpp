f0fd7620d32e707fe5757ef4a7ba45f2b21e9c33
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2024-05-26 16:02:45 +0530

    Time: 18 ms (57.74%), Space: 16.1 MB (64.16%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    int v[101];
     bool isBipartite(vector<vector<int>>& graph) {
-        int len =graph.size();
-        int Q[101] = {0};
-        for(int i=0;i<len;i++){
-            if(v[i]==1 ||v[i]==-1)continue;
-            int head = 0;
-            int tail = 0;
-            Q[tail++] = i;
-            v[i]=1;
-            while(head != tail){
-                int curr = Q[head];
-                head++;
-                for(int next:graph[curr]){
-                    if(v[next]==0){
-                        v[next]=-v[curr];
-                        Q[tail++] = next;
+        int n = size(graph);
+        vector<int>color(n,0);
+        queue<int>q;
+        for(int i = 0; i < n; i++){
+            if(color[i] == 1 or color[i] == -1)continue;
+            q.push(i);
+            color[i] = 1;
+            while(!q.empty()){
+                int curr = q.front();
+                q.pop();
+                for(auto neigh : graph[curr]){
+                    if(color[neigh] == 0){
+                        color[neigh] = -color[curr];
+                        q.push(neigh);
                     }
-                    if(v[next]==v[curr])return false;
+                    if(color[neigh] == color[curr])return false;
                 }
             }
         }
