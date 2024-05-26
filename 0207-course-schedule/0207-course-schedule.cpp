4090b4f930e93071746d23ff6113ab99e05a1507
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2024-05-26 18:12:37 +0530

    Time: 15 ms (70.71%), Space: 16.7 MB (91.45%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,4 +1,4 @@
-class Solution { //BFS
+class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
         vector<vector<int>> G(n);
@@ -11,38 +11,4 @@ public:
                 if (--degree[j] == 0) bfs.push_back(j);
         return bfs.size() == n;
     }
-   
 };
-
-//DFS
-// class Solution {
-// public:
-//     //In visited: 0->not explored, 1->being explored, 2->fully explored
-//     bool iscycle(vector<int> adj[],vector<int> &vis,int id){
-//         if(vis[id]==1)
-//             return true;
-//         if(vis[id]==0){
-//             vis[id]=1;
-//             for(auto edge : adj[id]){
-//                 if(iscycle(adj,vis,edge))
-//                     return true;
-//             }
-//         }
-//         vis[id] = 2;
-//         return false;
-//     }
-//     bool canFinish(int n, vector<vector<int>>& pre) {
-//         vector<int> adj[n];
-//         for(auto edge : pre)
-//             adj[edge[1]].push_back(edge[0]);
-//         vector<int> vis(n,0);
-        
-//         for(int i=0;i<n;i++){
-//             if(iscycle(adj,vis,i))
-//                 return false;
-//         }
-//         return true;
-//     }
-// };
-
-
