b6b0108ad07875057e67d3582b1e63719fc21446
Shyamji07 <72574314+Shyamji07@users.noreply.github.com>
2024-05-26 11:53:09 +0530

    Time: 862 ms (38.46%), Space: 325.2 MB (7.69%) - LeetHub

---
+++ 3164-find-the-number-of-good-pairs-ii/3164-find-the-number-of-good-pairs-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        unordered_map<int,long long> a, b; 
+        for (int num : nums2) {
+            b[num]++; 
+        } 
+        int mx=0;
+        for (int num : nums1) {
+            if(num%k!=0){ continue;}
+            a[num]++;
+            mx=max(mx,num);
+        }
+        vector<long long> kp;
+        
+        long long gp = 0;
+
+        for(auto it:b){
+            long long h=k*it.first;
+            long long jp=0; 
+            int op=1;
+            while(op*h<=mx){
+                jp+=a[op*h];
+                op++;
+            }
+            gp+=jp*it.second;
+        }
+
+        return gp;
+    }
+};
